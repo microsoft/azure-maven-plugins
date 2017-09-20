fc2671a21f43fba99956477729eeb95b09274dd8
Kevin Zhao <kvn.zhl@gmail.com>
2017-09-20 16:44:43 +0800

    support more bindings for function.json generation (#52)

---
+++ azure-functions-maven-plugin/src/main/java/com/microsoft/azure/maven/function/bindings/DocumentDBBinding.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.maven.function.bindings;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.microsoft.azure.serverless.functions.annotation.DocumentDBInput;
+import com.microsoft.azure.serverless.functions.annotation.DocumentDBOutput;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class DocumentDBBinding extends BaseBinding {
+    private String databaseName = "";
+
+    private String collectionName = "";
+
+    private String id = "";
+
+    private String sqlQuery = "";
+
+    private String connection = "";
+
+    private boolean createIfNotExists = false;
+
+    public DocumentDBBinding(final DocumentDBInput dbInput) {
+        setDirection("in");
+        setType("documentdb");
+        setName(dbInput.name());
+
+        databaseName = dbInput.databaseName();
+        collectionName = dbInput.collectionName();
+        id = dbInput.id();
+        sqlQuery = dbInput.sqlQuery();
+        connection = dbInput.connection();
+    }
+
+    public DocumentDBBinding(final DocumentDBOutput dbOutput) {
+        setDirection("out");
+        setType("documentdb");
+        setName(dbOutput.name());
+
+        databaseName = dbOutput.databaseName();
+        collectionName = dbOutput.collectionName();
+        connection = dbOutput.connection();
+        createIfNotExists = dbOutput.createIfNotExists();
+    }
+
+    @JsonGetter
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    @JsonGetter
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    @JsonGetter
+    public String getId() {
+        return id;
+    }
+
+    @JsonGetter
+    public String getSqlQuery() {
+        return sqlQuery;
+    }
+
+    @JsonGetter
+    public String getConnection() {
+        return connection;
+    }
+
+    @JsonGetter
+    public boolean isCreateIfNotExists() {
+        return createIfNotExists;
+    }
+}
---
+++ azure-functions-maven-plugin/src/main/java/com/microsoft/azure/maven/function/bindings/NotificationHubBinding.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.maven.function.bindings;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.microsoft.azure.serverless.functions.annotation.NotificationHubOutput;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class NotificationHubBinding extends BaseBinding {
+    private String tagExpression = "";
+
+    private String hubName = "";
+
+    private String connection = "";
+
+    private String platform = "";
+
+    public NotificationHubBinding(final NotificationHubOutput hubOutput) {
+        setDirection("out");
+        setType("notificationHub");
+        setName(hubOutput.name());
+
+        tagExpression = hubOutput.tagExpression();
+        hubName = hubOutput.hubName();
+        connection = hubOutput.connection();
+        platform = hubOutput.platform();
+    }
+
+    @JsonGetter
+    public String getTagExpression() {
+        return tagExpression;
+    }
+
+    @JsonGetter
+    public String getHubName() {
+        return hubName;
+    }
+
+    @JsonGetter
+    public String getConnection() {
+        return connection;
+    }
+
+    @JsonGetter
+    public String getPlatform() {
+        return platform;
+    }
+}
