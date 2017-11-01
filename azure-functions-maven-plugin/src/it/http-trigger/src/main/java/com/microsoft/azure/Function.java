d221bd90ca95b1d70f78f443248b9cd1f9199155
Kevin Zhao <kvn.zhl@gmail.com>
2017-11-01 02:47:35 -0500

    integrate official Azure Function templates with Functions Plugin (#59)

---
+++ azure-functions-maven-plugin/src/it/http-trigger/src/main/java/com/microsoft/azure/Function.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure;
+
+import com.microsoft.azure.serverless.functions.annotation.*;
+import com.microsoft.azure.serverless.functions.*;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class Function {
+    @FunctionName("hello")
+    public HttpResponseMessage httpHandler(
+            @HttpTrigger(name = "req", methods = {"get", "post"}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage request,
+            final ExecutionContext context
+    ) {
+        context.getLogger().info("Java HTTP trigger processed a HTTP request.");
+
+        // Parse query parameter
+        String name = request.getQueryParameters().get("name");
+
+        if (name == null) {
+            // Get request body
+            Object body = request.getBody();
+            if (body != null) {
+                name = body.toString();
+            }
+        }
+
+        if (name == null) {
+            return request.createResponse(400, "Please pass a name on the query string or in the request body");
+        } else {
+            return request.createResponse(200, "Hello, " + name);
+        }
+    }
+}
