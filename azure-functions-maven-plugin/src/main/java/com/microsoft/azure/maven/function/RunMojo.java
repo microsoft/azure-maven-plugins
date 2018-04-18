c2ea344d837fb320d97d599ea7b5dd24407eb5d6
Sheng Chen <sheche@microsoft.com>
2018-04-18 13:07:59 +0800

    extract out run command logic

--- azure-functions-maven-plugin/src/main/java/com/microsoft/azure/maven/function/RunMojo.java
+++ azure-functions-maven-plugin/src/main/java/com/microsoft/azure/maven/function/RunMojo.java
@@ -6,19 +6,14 @@
 
 package com.microsoft.azure.maven.function;
 
-import org.apache.commons.lang3.SystemUtils;
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.codehaus.plexus.util.IOUtil;
-import org.codehaus.plexus.util.StringUtils;
 
 import java.io.File;
-import java.io.InputStream;
-import java.lang.ProcessBuilder.Redirect;
-import java.util.Arrays;
-import java.util.List;
 
-import static java.lang.String.format;
+import com.microsoft.azure.maven.function.handlers.CommandHandler;
+import com.microsoft.azure.maven.function.handlers.CommandHandlerImpl;
+import com.microsoft.azure.maven.function.utils.CommandUtils;
 
 /**
  * Run Azure Java Functions locally. Azure Functions Core Tools is required to be installed first.
@@ -34,118 +29,61 @@ public class RunMojo extends AbstractFunctionMojo {
     public static final String RUN_FUNCTIONS_FAILURE = "Failed to run Azure Functions. Please checkout console output.";
     public static final String START_RUN_FUNCTIONS = "Starting running Azure Functions...";
 
-    public static final String WINDOWS_HOST_START = "cd /D %s && func host start";
-    public static final String LINUX_HOST_START = "cd %s; func host start";
+    public static final String FUNC_HOST_START_CMD = "func host start";
 
     //region Entry Point
 
     @Override
     protected void doExecute() throws Exception {
+        final CommandHandler commandHandler = new CommandHandlerImpl(this.getLog());
+
         checkStageDirectoryExistence();
 
-        checkRuntimeExistence();
+        checkRuntimeExistence(commandHandler);
 
-        runFunctions();
+        runFunctions(commandHandler);
     }
 
     protected void checkStageDirectoryExistence() throws Exception {
-        runCommand(getCheckStageDirectoryCommand(), false, getDefaultValidReturnCodes(), STAGE_DIR_NOT_FOUND);
+        final File file = new File(getDeploymentStageDirectory());
+        if (!file.exists() || !file.isDirectory()) {
+            throw new MojoExecutionException(STAGE_DIR_NOT_FOUND);
+        }
         info(STAGE_DIR_FOUND + getDeploymentStageDirectory());
     }
 
-    protected void checkRuntimeExistence() throws Exception {
-        runCommand(getCheckRuntimeCommand(), false, getDefaultValidReturnCodes(), RUNTIME_NOT_FOUND);
+    protected void checkRuntimeExistence(final CommandHandler handler) throws Exception {
+        handler.runCommandWithReturnCodeCheck(
+                getCheckRuntimeCommand(),
+                false, /* showStdout */
+                null, /* workingDirectory */
+                CommandUtils.getDefaultValidReturnCodes(),
+                RUNTIME_NOT_FOUND
+        );
         info(RUNTIME_FOUND);
     }
 
-    protected void runFunctions() throws Exception {
+    protected void runFunctions(final CommandHandler handler) throws Exception {
         info(START_RUN_FUNCTIONS);
-        runCommand(getStartFunctionHostCommand(), true, getValidReturnCodes(), RUN_FUNCTIONS_FAILURE);
+        handler.runCommandWithReturnCodeCheck(
+                getStartFunctionHostCommand(),
+                true, /* showStdout */
+                getDeploymentStageDirectory(),
+                CommandUtils.getValidReturnCodes(),
+                RUN_FUNCTIONS_FAILURE
+        );
     }
 
     //endregion
 
     //region Build commands
 
-    protected String[] getCheckStageDirectoryCommand() {
-        final String command = format(isWindows() ? "cd /D %s" : "cd %s", getDeploymentStageDirectory());
-        return buildCommand(command);
+    protected String getCheckRuntimeCommand() {
+        return "func";
     }
 
-    protected String[] getCheckRuntimeCommand() {
-        return buildCommand("func");
-    }
-
-    protected String[] getStartFunctionHostCommand() {
-        final String command = format(getStartFunctionHostTemplate(), getDeploymentStageDirectory());
-        return buildCommand(command);
-    }
-
-    protected String getStartFunctionHostTemplate() {
-        return isWindows() ? WINDOWS_HOST_START : LINUX_HOST_START;
-    }
-
-    protected String[] buildCommand(final String command) {
-        return isWindows() ?
-                new String[]{"cmd.exe", "/c", command} :
-                new String[]{"sh", "-c", command};
-    }
-
-    //endregion
-
-    //region Helper methods
-
-    protected boolean isWindows() {
-        return SystemUtils.IS_OS_WINDOWS;
-    }
-
-    protected List<Long> getDefaultValidReturnCodes() {
-        return Arrays.asList(0L);
-    }
-
-    protected List<Long> getValidReturnCodes() {
-        return isWindows() ?
-                // Windows return code of CTRL-C is 3221225786
-                Arrays.asList(0L, 3221225786L) :
-                // Linux return code of CTRL-C is 130
-                Arrays.asList(0L, 130L);
-    }
-
-    protected void runCommand(final String[] command, final boolean showStdout, final List<Long> validReturnCodes,
-                              final String errorMessage) throws Exception {
-        debug("Executing command: " + StringUtils.join(command, " "));
-
-        final Redirect redirect = getStdoutRedirect(showStdout);
-        final Process process = new ProcessBuilder(command)
-                .redirectOutput(redirect)
-                .redirectErrorStream(true)
-                .start();
-
-        process.waitFor();
-
-        handleExitValue(process.exitValue(), validReturnCodes, errorMessage, process.getInputStream());
-    }
-
-    protected Redirect getStdoutRedirect(boolean showStdout) {
-        return showStdout ? Redirect.INHERIT : Redirect.PIPE;
-    }
-
-    protected void handleExitValue(int exitValue, final List<Long> validReturnCodes, final String errorMessage,
-                                   final InputStream inputStream) throws Exception {
-        debug("Process exit value: " + exitValue);
-        if (!validReturnCodes.contains(Integer.toUnsignedLong(exitValue))) {
-            // input stream is a merge of standard output and standard error of the sub-process
-            showErrorIfAny(inputStream);
-            error(errorMessage);
-            throw new Exception(errorMessage);
-        }
-    }
-
-    protected void showErrorIfAny(final InputStream inputStream) throws Exception {
-        if (inputStream != null) {
-            final String input = IOUtil.toString(inputStream);
-            error(StringUtils.strip(input, "\n"));
-        }
+    protected String getStartFunctionHostCommand() {
+        return FUNC_HOST_START_CMD;
     }
 
     //endregion
