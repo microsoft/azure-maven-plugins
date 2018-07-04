27c02e107559be58560fe6f38f626e3d3eade368
Summer <victoriasqq@gmail.com>
2018-07-04 10:59:37 +0800

    add integration tests for linux and windows, war and jar deploy (#210)

---
+++ azure-webapp-maven-plugin/src/it/2-private-docker-hub/cleanup.groovy
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+// Clean up resources created in test
+def clientId = System.getenv("CLIENT_ID")
+def tenantId = System.getenv("TENANT_ID")
+def key = System.getenv("KEY")
+def commands = [
+    "az login --service-principal -u ${clientId}  -p ${key} --tenant ${tenantId}",
+    "az group delete -y -n maven-webapp-it-rg-2 --no-wait",
+    "az logout"
+]
+
+if (System.properties['os.name'].contains('Windows')) {
+    for (c in commands) {
+        println "cmd /c ${c}".execute().text
+    }
+} else {
+    for (c in commands) {
+        println "bash -c ${c}".execute().text
+    }
+}
