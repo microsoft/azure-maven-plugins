4eca5f27821c70345968d46c0d5d7a485c50e78a
Summer Sun <victoriasqq@gmail.com>
2018-07-10 14:42:30 +0800

    add integration test for deployment slot

--- azure-webapp-maven-plugin/src/test/java/com/microsoft/azure/maven/webapp/utils/TestUtils.java
+++ azure-webapp-maven-plugin/src/test/java/com/microsoft/azure/maven/webapp/utils/TestUtils.java
@@ -9,26 +9,56 @@ package com.microsoft.azure.maven.webapp.utils;
 import java.io.IOException;
 
 public class TestUtils {
+    private static final String clientId = System.getenv("CLIENT_ID");
+    private static final String tenantId = System.getenv("TENANT_ID");
+    private static final String key = System.getenv("KEY");
+
+    private static final boolean isWindows = System.getProperty("os.name").contains("Windows");
+
+    private static void azureLogin() throws IOException, InterruptedException {
+        final String azLoginTemplate = "az login --service-principal -u %s -p %s --tenant %s";
+        Runtime.getRuntime().exec(formatCommand(azLoginTemplate, new String[]{clientId, key, tenantId})).waitFor();
+    }
+
+    private static void azureLogout() throws IOException, InterruptedException {
+        Runtime.getRuntime().exec(formatCommand("%s", new String[]{"az logout"})).waitFor();
+    }
+
     public static void deleteAzureResourceGroup(String resourceGroupName, boolean waitForOperationFinish)
             throws InterruptedException, IOException {
-        final String clientId = System.getenv("CLIENT_ID");
-        final String tenantId = System.getenv("TENANT_ID");
-        final String key = System.getenv("KEY");
-        final String azLoginTemplate = "az login --service-principal -u %s -p %s --tenant %s";
-        final String azDelteTemplate = "az group delete -y -n %s%s";
 
-        final String[] commands = { String.format(azLoginTemplate, clientId, key, tenantId),
-                String.format(azDelteTemplate, resourceGroupName, waitForOperationFinish ? "" : " --no-wait"),
-                "az logout" };
+        final String azResourceGroupDeleteTemplate = "az group delete -y -n %s%s";
+        azureLogin();
+        Runtime.getRuntime().exec(formatCommand(azResourceGroupDeleteTemplate,
+                new String[]{resourceGroupName, waitForOperationFinish ? "" : " --no-wait"})).waitFor();
+        azureLogout();
+    }
 
-        if (System.getProperty("os.name").contains("Windows")) {
-            for (final String command : commands) {
-                Runtime.getRuntime().exec(String.format("cmd /c %s", command)).waitFor();
-            }
+    public static void createAzureWebApp(final String resourceGroupName, final String location,
+                                         final String servicePlanName, final String webAppName)
+            throws IOException, InterruptedException {
+        final String azResourceGroupCreateTemplate = "az group create --name %s --location %s";
+        final String azAppServicePlanCreateTemplate =
+                "az appservice plan create --resource-group %s --name %s --sku S1";
+        final String azWebAppCreateTemplate = "az webapp create --resource-group %s --name %s --plan %s";
+        azureLogin();
+        Runtime.getRuntime()
+                .exec(formatCommand(azResourceGroupCreateTemplate, new String[]{resourceGroupName, location}))
+                .waitFor();
+        Runtime.getRuntime().exec(
+                formatCommand(azAppServicePlanCreateTemplate, new String[]{resourceGroupName, servicePlanName}))
+                .waitFor();
+        Runtime.getRuntime().exec(
+                formatCommand(azWebAppCreateTemplate, new String[]{resourceGroupName, webAppName, servicePlanName}))
+                .waitFor();
+        azureLogout();
+    }
+
+    private static String formatCommand(String template, String[] args) {
+        if (isWindows) {
+            return String.format("cmd /c %s", String.format(template, args));
         } else {
-            for (final String command : commands) {
-                Runtime.getRuntime().exec(String.format("bash -c %s", command)).waitFor();
-            }
+            return String.format("bash -c %s", String.format(template, args));
         }
     }
 }
