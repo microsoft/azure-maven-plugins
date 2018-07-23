aec68d475999c456ae391659863a4439d13d7831
Sheng Chen <sheche@microsoft.com>
2018-07-23 12:49:34 +0800

    add license header

--- azure-functions-maven-plugin/src/it/1-storage-queue-trigger/src/main/java/com/microsoft/azure/Function.java
+++ azure-functions-maven-plugin/src/it/1-storage-queue-trigger/src/main/java/com/microsoft/azure/Function.java
@@ -1,4 +1,10 @@
-package test;
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure;
 
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
