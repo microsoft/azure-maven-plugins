b61e1186dac9d1b8ca192144ee473aa55976ff77
Summer Sun <victoriasqq@gmail.com>
2018-07-30 09:18:40 +0800

    fix import

--- azure-webapp-maven-plugin/src/test/java/com/microsoft/azure/maven/webapp/handlers/NullRuntimeHandlerImplTest.java
+++ azure-webapp-maven-plugin/src/test/java/com/microsoft/azure/maven/webapp/handlers/NullRuntimeHandlerImplTest.java
@@ -8,6 +8,7 @@ package com.microsoft.azure.maven.webapp.handlers;
 
 import com.microsoft.azure.management.appservice.WebApp;
 import com.microsoft.azure.management.appservice.WebApp.Update;
+import com.microsoft.azure.management.appservice.implementation.SiteInner;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.junit.Before;
 import org.junit.Test;
