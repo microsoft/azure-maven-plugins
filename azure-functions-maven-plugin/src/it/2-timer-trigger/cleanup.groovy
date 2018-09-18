9663a36675f102554b491206d286d7c5eb28d62d
hanxiao <hanli@microsoft.com>
2018-09-18 14:51:27 +0800

    Add ci test case for timer trigger

---
+++ azure-functions-maven-plugin/src/it/2-timer-trigger/cleanup.groovy
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+import com.microsoft.azure.maven.function.invoker.storage.StorageQueueClient
+import com.microsoft.azure.maven.function.invoker.CommonUtils
+
+// Get Connection String for the Function App binding Storage Account
+String azQueryValue = CommonUtils.executeCommand("az functionapp config appsettings list -n \"maven-functions-it-${timestamp}-2\" -g \"maven-functions-it-rg-2\" --query \"[?name=='AzureWebJobsStorage'].value\"")
+String storageConnectionString = azQueryValue.split("\"")[1]
+
+StorageQueueClient queueClient = new StorageQueueClient()
+queueClient.init(storageConnectionString)
+queueClient.createQueueWithName("out")
+
+CommonUtils.runVerification(new Runnable() {
+    @Override
+    void run() {
+        sleep(10000)
+        String msg = queueClient.peekNextMessageFrom("out")
+        assert msg == "CITEST"
+    }
+})
+
+// Clean up resources created in test
+CommonUtils.deleteAzureResourceGroup("maven-functions-it-rg-2", false)
+return true
