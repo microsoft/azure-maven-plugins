02e678930e0d90035b3df8204e1288268c280366
hanxiao <hanli@microsoft.com>
2018-11-01 18:49:24 +0800

    Resolve comments

--- azure-functions-maven-plugin/src/main/java/com/microsoft/azure/maven/function/AbstractFunctionMojo.java
+++ azure-functions-maven-plugin/src/main/java/com/microsoft/azure/maven/function/AbstractFunctionMojo.java
@@ -19,25 +19,25 @@ import java.io.File;
 
 public abstract class AbstractFunctionMojo extends AbstractAppServiceMojo {
 
-    private static final String JDK_VERSION_ERROR = "Your JDK Version is %s ,which doesn't fit azure maven " +
-            "function plugin, for now we only support JDK 8";
+    private static final String JDK_VERSION_ERROR = "Local JDK Version %s does not meet the maven " +
+            "plugin requirement. The supported version is JDK 8";
 
     //region Properties
     /**
      * App Service pricing tier, which will only be used to create Functions App at the first time.<br/>
      * Below is the list of supported pricing tier:
      * <ul>
-     * <li>F1</li>
-     * <li>D1</li>
-     * <li>B1</li>
-     * <li>B2</li>
-     * <li>B3</li>
-     * <li>S1</li>
-     * <li>S2</li>
-     * <li>S3</li>
-     * <li>P1</li>
-     * <li>P2</li>
-     * <li>P3</li>
+     *     <li>F1</li>
+     *     <li>D1</li>
+     *     <li>B1</li>
+     *     <li>B2</li>
+     *     <li>B3</li>
+     *     <li>S1</li>
+     *     <li>S2</li>
+     *     <li>S3</li>
+     *     <li>P1</li>
+     *     <li>P2</li>
+     *     <li>P3</li>
      * </ul>
      */
     @Parameter(property = "functions.pricingTier")
