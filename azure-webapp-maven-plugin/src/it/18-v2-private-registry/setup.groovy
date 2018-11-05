4b63d0e3c38565cd5af84c93bb1dd5cfccb30e63
Summer <victoriasqq@gmail.com>
2018-11-05 17:01:31 +0800

    Add integration tests for V2 schema version (#469)

---
+++ azure-webapp-maven-plugin/src/it/18-v2-private-registry/setup.groovy
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+import com.microsoft.azure.maven.webapp.utils.TestUtils
+
+TestUtils.azureLogin()
+
+TestUtils.deleteAzureResourceGroup("maven-webapp-it-rg-18", true)
+
+return true
