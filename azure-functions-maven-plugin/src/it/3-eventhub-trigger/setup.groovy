84192d806b4870f4e494d2548ddadc1e1988ba7d
Hanxiao Liu <hanli@microsoft.com>
2018-11-21 13:05:33 +0800

    Add Event Hub E2E test (#428)

---
+++ azure-functions-maven-plugin/src/it/3-eventhub-trigger/setup.groovy
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+import com.microsoft.azure.maven.function.invoker.CommonUtils
+
+CommonUtils.azureLogin()
+
+CommonUtils.deleteAzureResourceGroup("maven-functions-it-${timestamp}-rg-3", true)
+
+return true
