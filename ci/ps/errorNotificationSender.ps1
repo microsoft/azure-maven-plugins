954c6c18e10e23bf93ffecfb2b358e254a755611
Hanxiao Liu <hanli@microsoft.com>
2019-03-19 14:49:48 +0800

    Update CI notification email format (#597)

--- ci/ps/errorNotificationSender.ps1
+++ ci/ps/errorNotificationSender.ps1
@@ -1,8 +1,6 @@
 Param(
-    [string] $link,
-    [string[]] $logs
+    [string] $project
 )
-
 $MailSenderType = @' 
 namespace WindowsAPILib
 {
@@ -58,42 +56,98 @@ namespace WindowsAPILib
 '@
 Add-Type -TypeDefinition $MailSenderType -Language CSharp;
 
-Function GetEmailContent($link, $logs) {
-    $logFile = Get-Content $logs;
-    $errorLog = $logFile -match "\[error\]";
-    $errorContent = [string]::join("<br/>", $errorLog)
-    $pageBody="
-<html>
-    <span>Please check the CI log in the attachment</span>
-    <span>If you want to get more information, please refer <a href='$link'>Appveyor Job Log</a></span>
-    <br/>
-    <br/>
-    {$errorContent}
-    <br/>
-    <br/>
-    <span>This report is generated automatically. For any suggestions or issues, please contact hanli@microsoft.com.</span>
-</html>
+Function CreateEventObject($reportXML) {
+    $XMLObject = ([xml](Get-Content $reportXML)).'build-job'
+    $result = [PSCustomObject]@{
+        Project = $XMLObject.project.SubString(0, $XMLObject.project.Length - 8) # Remove  /pom.xml from project name
+        Result  = IF ($XMLObject.result -eq "success") {"success"} Else {"fail"}
+        Time    = $XMLObject.time
+        Message = $XMLObject.failureMessage
+    }
+    return $result;
+}
+
+Function GetEmailContent($project, $resultList) {
+    $link = $Env:FUNCTION_URL
+    $resultTable = [string]::join(" ", ($resultList | ForEach-Object { GetReportRow $_ }))  
+    $pageBody = "
+    <html>
+    <head>
+      <style>
+        table {
+          border-collapse: collapse;
+        }
+    
+        th,
+        td {
+          text-align: left;
+          padding: 8px;
+        }
+        
+        tr:nth-child(even) {background-color: #f2f2f2;}
+    
+        tr:hover {
+          background-color: #f5f5f5;
+        }
+      </style>
+    </head>
+    <body>
+      <p>Please check the CI log for azure-functions-maven-plugin in the attachment</p>
+      <p>If you want to get more information, please refer <a href='$link'>Appveyor Job Log</a></p>
+      <br />
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Project</th>
+              <th>Result</th>
+              <th>Time</th>
+              <th>Message</th>
+            </tr>
+          </thead>
+          <tbody>
+            $resultTable
+          </tbody>
+        </table>
+      </div>
+      <br />
+      <span>This report is generated automatically. For any suggestions or issues, please contact
+        hanli@microsoft.com.</span>
+    </body>
+    </html>
 "
     return $pageBody;
+}
+
+Function GetReportRow($report) {
+    $resultWithColor = IF ($report.result -eq "success") {"<font color='green'>success</font>"} Else {"<font color='red'>fail</font>"}
+    $result = "<tr><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td></tr>" -f $report.Project, $resultWithColor, $report.Time, $report.Message
+    return $result
 }
 
 #back to base folder
 $base = $Env:APPVEYOR_BUILD_FOLDER;
 cd $base;
+
 #send email
-$title="Azure Function Maven Plugin CI Issue";
-$mailto=New-Object System.Collections.Generic.List[System.String];
+$title = "Azure Maven Plugin CI Issue";
+$mailto = New-Object System.Collections.Generic.List[System.String];
 $mailto.Add("andxu@microsoft.com");
 $mailto.Add("Sheng.Chen@microsoft.com");
 $mailto.Add("Rome.Li@microsoft.com");
-$mailcc=New-Object System.Collections.Generic.List[System.String];
+$mailcc = New-Object System.Collections.Generic.List[System.String];
 $mailcc.Add("hanli@microsoft.com");
-$sender = 'insvsc@microsoft.com';
+$sender = "insvsc@microsoft.com";
+$senderPassword = $env:EMAIL_PASSWORD
 
-$pageBody=GetEmailContent $link $logs
-$senderPassword=$env:EMAIL_PASSWORD
+$reports = Get-ChildItem "$base\$project\target\invoker-reports\" -Filter "*.xml" | ForEach-Object { CreateEventObject "$base\$project\target\invoker-reports\$_" };
+$pageBody = GetEmailContent $project $reports
 $attachments = New-Object System.Collections.Generic.List[System.String];
-foreach($logFile in $logs) {
-    $attachments.Add((Get-Item $logFile).FullName); 
+foreach ($report in $reports) {
+    IF ($report.Result -eq "fail") {
+        Rename-Item ("{0}\{1}\target\it\{2}\build.log" -f $base, $project, $report.Project) ("{0}-build.log" -f $report.Project)
+        $attachments.Add(("{0}\{1}\target\it\{2}\{2}-build.log" -f $base, $project, $report.Project )); 
+    }
 }
+
 [WindowsAPILib.OutlookDotComMail]::SendMail($mailto, $mailcc, $title, $sender, $senderPassword, $attachments, $pageBody);
