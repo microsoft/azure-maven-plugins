e0b0ffa69023c51a420a4c8ac228cd9e218c1cba
Hanxiao Liu <hanli@microsoft.com>
2019-06-18 15:43:45 +0800

    Fix hint for webapp:config (#675)

--- azure-maven-plugin-lib/src/main/java/com/microsoft/azure/maven/queryer/MavenPluginQueryer.java
+++ azure-maven-plugin-lib/src/main/java/com/microsoft/azure/maven/queryer/MavenPluginQueryer.java
@@ -6,10 +6,12 @@
 
 package com.microsoft.azure.maven.queryer;
 
+import org.apache.commons.collections4.BidiMap;
 import org.apache.maven.plugin.MojoFailureException;
 import org.codehaus.plexus.util.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
@@ -33,6 +35,14 @@ public abstract class MavenPluginQueryer {
         }
         final ArrayList<String> options = new ArrayList<>(optionSet);
         return assureInputFromUser(attribute, defaultValueForAttribute, options, prompt);
+    }
+
+    public String assureInputFromUser(String attribute, String defaultValue,
+                                      BidiMap<String, String> options, String prompt) throws MojoFailureException {
+        final String defaultDisplayName = options.getKey(defaultValue);
+        final List<String> displayNames = new ArrayList<>(options.keySet());
+        Collections.sort(displayNames);
+        return options.get(assureInputFromUser(attribute, defaultDisplayName, displayNames, prompt));
     }
 
     protected boolean validateInputByOptions(String input, List<String> options) {
