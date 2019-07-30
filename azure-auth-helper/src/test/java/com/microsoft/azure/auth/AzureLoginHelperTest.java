22cbbd9adb6b8467128737cb4ef39c705cf70234
andxu <andxu@microsoft.com>
2019-07-30 15:22:11 +0800

    1. add oauth/device login 2. add logic to create token credentials from azure:login, azure cli and cloud shell.

---
+++ azure-auth-helper/src/test/java/com/microsoft/azure/auth/AzureLoginHelperTest.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.auth;
+
+import com.microsoft.azure.AzureEnvironment;
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class AzureLoginHelperTest {
+    @Test
+    public void testAuthorizationUrl() throws Exception {
+        String url = AzureLoginHelper.authorizationUrl(AzureEnvironment.AZURE, "http://localhost:4663");
+        Map<String, String> queryMap = splitQuery(url);
+        assertEquals(Constants.CLIENT_ID, queryMap.get("client_id"));
+        assertEquals("http://localhost:4663", queryMap.get("redirect_uri"));
+        assertEquals("code", queryMap.get("response_type"));
+        assertEquals("select_account", queryMap.get("prompt"));
+        assertEquals(AzureEnvironment.AZURE.activeDirectoryResourceId(), queryMap.get("resource"));
+
+        url = AzureLoginHelper.authorizationUrl(AzureEnvironment.AZURE_CHINA, "http://localhost:4664");
+        queryMap = splitQuery(url);
+        assertEquals(Constants.CLIENT_ID, queryMap.get("client_id"));
+        assertEquals("http://localhost:4664", queryMap.get("redirect_uri"));
+        assertEquals("code", queryMap.get("response_type"));
+        assertEquals("select_account", queryMap.get("prompt"));
+        assertEquals(AzureEnvironment.AZURE_CHINA.activeDirectoryResourceId(), queryMap.get("resource"));
+    }
+
+    @Test
+    public void tesAuthorizationUrlInvalidParameter() throws Exception {
+        try {
+            AzureLoginHelper.authorizationUrl(null, "http://localhost:4663");
+            fail("Should throw IAE when env is null.");
+        } catch (IllegalArgumentException e) {
+            // ignore
+        }
+
+        try {
+            AzureLoginHelper.authorizationUrl(AzureEnvironment.AZURE_CHINA, "");
+            fail("Should throw IAE when redirectUrl is empty.");
+        } catch (IllegalArgumentException e) {
+            // ignore
+        }
+    }
+
+    @Test
+    public void testBaseURL() {
+        String baseUrl = AzureLoginHelper.baseURL(AzureEnvironment.AZURE);
+        assertEquals("https://login.microsoftonline.com/common", baseUrl);
+        baseUrl = AzureLoginHelper.baseURL(AzureEnvironment.AZURE_US_GOVERNMENT);
+        assertEquals("https://login.microsoftonline.us/common", baseUrl);
+    }
+
+    private static Map<String, String> splitQuery(String url) throws UnsupportedEncodingException, MalformedURLException {
+        final Map<String, String> queryPairs = new LinkedHashMap<>();
+        final String query = new URL(url).getQuery();
+        final String[] pairs = query.split("&");
+        for (final String pair : pairs) {
+            final int idx = pair.indexOf("=");
+            queryPairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"),
+                    URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+        }
+        return queryPairs;
+    }
+
+}
