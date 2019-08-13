4f76bfd8a449b0f87f25b830c3e0974831d8eccc
Andy Xu(devdiv) <andxu@microsoft.com>
2019-08-13 13:42:07 +0800

    Add validation code to auth configuration (#727)
    * add validation to auth configuration

--- azure-auth-helper/src/main/java/com/microsoft/azure/auth/configuration/AuthConfiguration.java
+++ azure-auth-helper/src/main/java/com/microsoft/azure/auth/configuration/AuthConfiguration.java
@@ -6,6 +6,17 @@
 
 package com.microsoft.azure.auth.configuration;
 
+import com.microsoft.azure.auth.AzureAuthHelper;
+import com.microsoft.azure.maven.validation.ConfigurationProblem;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.microsoft.azure.maven.validation.ConfigurationProblem.Severity.ERROR;
+import static com.microsoft.azure.maven.validation.ConfigurationProblem.Severity.WARNING;
+
 public class AuthConfiguration {
     private String client;
     private String tenant;
@@ -87,6 +98,48 @@ public class AuthConfiguration {
 
     public void setHttpProxyPort(String httpProxyPort) {
         this.httpProxyPort = httpProxyPort;
+    }
+
+    public List<ConfigurationProblem> validate() {
+        final List<ConfigurationProblem> results = new ArrayList<>();
+        final String errorMessagePostfix = StringUtils.isNotBlank(serverId) ? "server: " + serverId + " in settings.xml." : "<auth> configuration.";
+        // The <serverId> check should be done out of this class.
+        if (StringUtils.isBlank(tenant)) {
+            results.add(new ConfigurationProblem("tenant", tenant, "Cannot find 'tenant' in " + errorMessagePostfix, ERROR));
+        }
+        if (StringUtils.isBlank(client)) {
+            results.add(new ConfigurationProblem("client", client, "Cannot find 'client' in " + errorMessagePostfix, ERROR));
+        }
+
+        if (StringUtils.isBlank(key) && StringUtils.isBlank(certificate)) {
+            results.add(new ConfigurationProblem("key", null, "Cannot find either 'key' or 'certificate' in " + errorMessagePostfix, ERROR));
+        }
+
+        if (StringUtils.isNotBlank(key) && StringUtils.isNotBlank(certificate)) {
+            results.add(new ConfigurationProblem(null, null, "It is illegal to specify both 'key' and 'certificate' in " + errorMessagePostfix, WARNING));
+        }
+
+        if (StringUtils.isNotBlank(environment) && !AzureAuthHelper.validateEnvironment(environment)) {
+            results.add(new ConfigurationProblem("environment", environment,
+                    String.format("Invalid environment string '%s' in " + errorMessagePostfix, environment), ERROR));
+        }
+
+        if ((StringUtils.isNotBlank(httpProxyHost) && StringUtils.isBlank(httpProxyPort)) ||
+                (StringUtils.isBlank(httpProxyHost) && StringUtils.isNotBlank(httpProxyPort))) {
+            results.add(new ConfigurationProblem(null, null, "'httpProxyHost' and 'httpProxyPort' must both be set if you want to use proxy.", ERROR));
+        }
+
+        if (StringUtils.isNotBlank(httpProxyPort)) {
+            if (!StringUtils.isNumeric(httpProxyPort)) {
+                results.add(new ConfigurationProblem("httpProxyPort", httpProxyPort,
+                        String.format("Invalid integer number for httpProxyPort: '%s'.", httpProxyPort), ERROR));
+            } else if (NumberUtils.toInt(httpProxyPort) <= 0 || NumberUtils.toInt(httpProxyPort) > 65535) {
+                results.add(new ConfigurationProblem("httpProxyPort", httpProxyPort,
+                        String.format("Invalid range of httpProxyPort: '%s', it should be a number between %d and %d", httpProxyPort, 1, 65535), ERROR));
+            }
+
+        }
+        return results;
     }
 
 }
