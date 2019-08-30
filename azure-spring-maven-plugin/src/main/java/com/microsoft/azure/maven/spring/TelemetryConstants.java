6c47541fddcd65ec1e9123ff65361a88eb82b4de
Hanxiao Liu <hanli@microsoft.com>
2019-08-30 15:56:01 +0800

    Fix checkstyle issue

--- azure-spring-maven-plugin/src/main/java/com/microsoft/azure/maven/spring/TelemetryConstants.java
+++ azure-spring-maven-plugin/src/main/java/com/microsoft/azure/maven/spring/TelemetryConstants.java
@@ -22,7 +22,6 @@ public class TelemetryConstants {
     public static final String TELEMETRY_KEY_IS_CREATE_DEPLOYMENT = "isCreateDeployment";
     public static final String TELEMETRY_KEY_IS_DEPLOYMENT_NAME_GIVEN = "isDeploymentNameGiven";
 
-
     public static final String TELEMETRY_VALUE_AUTH_POM_CONFIGURATION = "Pom Configuration";
     public static final String TELEMETRY_VALUE_AUTH_MAVEN_SERVER = "Maven Server";
     public static final String TELEMETRY_VALUE_AUTH_AZURE_MAVEN_PLUGIN = "Azure Maven Plugin";
