ebd29d4842189bd871cdac27055fc118a9d6a1f3
Andy Xu <andxu@microsoft.com>
2019-09-12 14:36:26 +0800

    Bump version 0.1.0-SNAPSHOT

--- azure-spring-cloud-maven-plugin/src/test/java/com/microsoft/azure/maven/spring/pom/PomXmlUpdaterTest.java
+++ azure-spring-cloud-maven-plugin/src/test/java/com/microsoft/azure/maven/spring/pom/PomXmlUpdaterTest.java
@@ -108,7 +108,7 @@ public class PomXmlUpdaterTest {
         final PluginDescriptor pd = mock(PluginDescriptor.class);
         when(pd.getGroupId()).thenReturn("com.microsoft.azure");
         when(pd.getArtifactId()).thenReturn("azure-spring-maven-plugin");
-        when(pd.getVersion()).thenReturn("0.0.1.SNAPSHOT");
+        when(pd.getVersion()).thenReturn("0.1.0.SNAPSHOT");
         final PomXmlUpdater updater = new PomXmlUpdater(project, pd);
         updater.updateSettings(app, deploy);
         final String updatedXml = FileUtils.readFileToString(tempFile);
