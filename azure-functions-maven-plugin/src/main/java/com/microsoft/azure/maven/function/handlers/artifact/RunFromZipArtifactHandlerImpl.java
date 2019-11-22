a7e9934d6608fe6f461fffae2476ea4c343efa01
Hanxiao Liu <hanli@microsoft.com>
2019-11-22 17:10:00 +0800

    Support run from zip deployment (#903)
    * Run from zip
    
    * Set run from zip as the default deployment method

---
+++ azure-functions-maven-plugin/src/main/java/com/microsoft/azure/maven/function/handlers/artifact/RunFromZipArtifactHandlerImpl.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.maven.function.handlers.artifact;
+
+import com.microsoft.azure.maven.deploytarget.DeployTarget;
+import com.microsoft.azure.maven.handlers.artifact.ZIPArtifactHandlerImpl;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import java.io.IOException;
+
+import static com.microsoft.azure.maven.function.Constants.APP_SETTING_WEBSITE_RUN_FROM_PACKAGE;
+
+public class RunFromZipArtifactHandlerImpl extends ZIPArtifactHandlerImpl {
+
+    protected static final String RUN_FROM_PACKAGE_VALUE = "1";
+
+    public static class Builder extends ZIPArtifactHandlerImpl.Builder {
+        @Override
+        protected RunFromZipArtifactHandlerImpl.Builder self() {
+            return this;
+        }
+
+        @Override
+        public RunFromZipArtifactHandlerImpl build() {
+            return new RunFromZipArtifactHandlerImpl(this);
+        }
+    }
+
+    protected RunFromZipArtifactHandlerImpl(Builder builder) {
+        super(builder);
+    }
+
+    @Override
+    public void publish(DeployTarget target) throws MojoExecutionException, IOException {
+        FunctionArtifactHelper.updateAppSetting(target, APP_SETTING_WEBSITE_RUN_FROM_PACKAGE, RUN_FROM_PACKAGE_VALUE);
+        super.publish(target);
+    }
+}
