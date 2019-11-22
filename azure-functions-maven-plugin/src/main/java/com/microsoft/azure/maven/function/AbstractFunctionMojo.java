bdba8df041654e68d6b7cc4e87297a846048548c
Julien Dubois <julien.dubois@gmail.com>
2019-11-22 14:04:53 +0100

    Set the FUNCTIONS_EXTENSION_VERSION property to ~2 (#898)

--- azure-functions-maven-plugin/src/main/java/com/microsoft/azure/maven/function/AbstractFunctionMojo.java
+++ azure-functions-maven-plugin/src/main/java/com/microsoft/azure/maven/function/AbstractFunctionMojo.java
@@ -30,6 +30,10 @@ public abstract class AbstractFunctionMojo extends AbstractAppServiceMojo {
     private static final String SET_FUNCTIONS_WORKER_RUNTIME = "Set function worker runtime to java";
     private static final String CHANGE_FUNCTIONS_WORKER_RUNTIME = "Function worker runtime doesn't " +
             "meet the requirement, change it from %s to java";
+    private static final String FUNCTIONS_EXTENSION_VERSION_NAME = "FUNCTIONS_EXTENSION_VERSION";
+    private static final String FUNCTIONS_EXTENSION_VERSION_VALUE = "~2";
+    private static final String SET_FUNCTIONS_EXTENSION_VERSION = "Functions extension version " +
+            "isn't configured, setting up the default value";
 
     //region Properties
     /**
@@ -80,15 +84,34 @@ public abstract class AbstractFunctionMojo extends AbstractAppServiceMojo {
     //region get App Settings
     @Override
     public Map getAppSettings() {
-        final Map result = super.getAppSettings();
-        final String workerRuntime = (String) result.get(FUNCTIONS_WORKER_RUNTIME_NAME);
-        if (StringUtils.isEmpty(workerRuntime)) {
-            info(SET_FUNCTIONS_WORKER_RUNTIME);
-        } else if (!FUNCTIONS_WORKER_RUNTIME_VALUE.equals(workerRuntime)) {
-            warning(String.format(CHANGE_FUNCTIONS_WORKER_RUNTIME, workerRuntime));
+        final Map settings = super.getAppSettings();
+        overrideDefaultAppSetting(settings, FUNCTIONS_WORKER_RUNTIME_NAME, SET_FUNCTIONS_WORKER_RUNTIME,
+                FUNCTIONS_WORKER_RUNTIME_VALUE, CHANGE_FUNCTIONS_WORKER_RUNTIME);
+        setDefaultAppSetting(settings, FUNCTIONS_EXTENSION_VERSION_NAME, SET_FUNCTIONS_EXTENSION_VERSION,
+                FUNCTIONS_EXTENSION_VERSION_VALUE);
+        return settings;
+    }
+
+    private void overrideDefaultAppSetting(Map result, String settingName, String settingIsEmptyMessage,
+                                           String settingValue, String changeSettingMessage) {
+
+        final String setting = (String) result.get(settingName);
+        if (StringUtils.isEmpty(setting)) {
+            info(settingIsEmptyMessage);
+        } else if (!setting.equals(settingValue)) {
+            warning(String.format(changeSettingMessage, setting));
         }
-        result.put(FUNCTIONS_WORKER_RUNTIME_NAME, FUNCTIONS_WORKER_RUNTIME_VALUE);
-        return result;
+        result.put(settingName, settingValue);
+    }
+
+    private void setDefaultAppSetting(Map result, String settingName, String settingIsEmptyMessage,
+                                        String settingValue) {
+
+        final String setting = (String) result.get(settingName);
+        if (StringUtils.isEmpty(setting)) {
+            info(settingIsEmptyMessage);
+            result.put(settingName, settingValue);
+        }
     }
     //endregion
 
