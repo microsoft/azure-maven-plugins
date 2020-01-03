ef8f67d4c963a348af92ef3af44aca7cb1b2a248
Hanxiao Liu <hanli@microsoft.com>
2020-01-03 13:18:14 +0800

    Check subscription and show more friendly error message

--- azure-maven-plugin-lib/src/main/java/com/microsoft/azure/maven/auth/AzureClientFactory.java
+++ azure-maven-plugin-lib/src/main/java/com/microsoft/azure/maven/auth/AzureClientFactory.java
@@ -7,6 +7,7 @@
 package com.microsoft.azure.maven.auth;
 
 import com.microsoft.azure.AzureEnvironment;
+import com.microsoft.azure.PagedList;
 import com.microsoft.azure.auth.AzureAuthHelper;
 import com.microsoft.azure.auth.AzureCredential;
 import com.microsoft.azure.auth.AzureTokenWrapper;
@@ -18,9 +19,11 @@ import com.microsoft.azure.auth.exception.InvalidConfigurationException;
 import com.microsoft.azure.common.logging.Log;
 import com.microsoft.azure.management.Azure;
 import com.microsoft.azure.management.Azure.Authenticated;
+import com.microsoft.azure.management.resources.Subscription;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 public class AzureClientFactory {
@@ -28,6 +31,8 @@ public class AzureClientFactory {
     private static final String SUBSCRIPTION_TEMPLATE = "Subscription : %s(%s)";
     public static final String SUBSCRIPTION_NOT_FOUND = "Subscription %s was not found in current account";
     public static final String NO_AVAILABLE_SUBSCRIPTION = "No available subscription found in current account";
+    public static final String SUBSCRIPTION_NOT_SPECIFIED = "Subscription ID was not specified, using the first subscription in current account," +
+            " please refer https://github.com/microsoft/azure-maven-plugins/wiki/Authentication#subscription for more information";
 
     public static Azure getAzureClient(AuthConfiguration auth, String subscriptionId) throws InvalidConfigurationException, IOException,
             AzureLoginFailureException, InterruptedException, ExecutionException {
@@ -54,11 +59,33 @@ public class AzureClientFactory {
             if (StringUtils.isEmpty(subscriptionId) && AzureAuthHelperLegacy.isInCloudShell()) {
                 subscriptionId = AzureAuthHelperLegacy.getSubscriptionOfCloudShell();
             }
-            return StringUtils.isEmpty(subscriptionId) ?
-                    authenticated.withDefaultSubscription() :
+            subscriptionId = StringUtils.isEmpty(subscriptionId) ? azureTokenCredentials.defaultSubscriptionId() : subscriptionId;
+            final Azure azureClient = StringUtils.isEmpty(subscriptionId) ? authenticated.withDefaultSubscription() :
                     authenticated.withSubscription(subscriptionId);
+            checkSubscription(azureClient, subscriptionId);
+            final Subscription subscription = azureClient.getCurrentSubscription();
+            Log.info(String.format(SUBSCRIPTION_TEMPLATE, subscription.displayName(), subscription.subscriptionId()));
+            return azureClient;
         }
 
         return null;
+    }
+
+    private static void checkSubscription(Azure azure, String targetSubscription) throws AzureLoginFailureException {
+        final PagedList<Subscription> subscriptions = azure.subscriptions().list();
+        subscriptions.loadAll();
+        if (subscriptions.size() == 0) {
+            throw new AzureLoginFailureException(NO_AVAILABLE_SUBSCRIPTION);
+        }
+        if (StringUtils.isEmpty(targetSubscription)) {
+            Log.warn(SUBSCRIPTION_NOT_SPECIFIED);
+            return;
+        }
+        final Optional<Subscription> targetSubscription = subscriptions.stream()
+                .filter(subscription -> StringUtils.equals(subscription.subscriptionId(), targetSubscription))
+                .findAny();
+        if (!targetSubscription.isPresent()) {
+            throw new AzureLoginFailureException(String.format(SUBSCRIPTION_NOT_FOUND, targetSubscription));
+        }
     }
 }
