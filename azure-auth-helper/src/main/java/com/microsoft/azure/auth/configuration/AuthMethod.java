d023f2d2e79e8867ac1a398d555ffcdc71fbcca8
Hanxiao Liu <hanli@microsoft.com>
2020-01-10 13:40:57 +0800

    Keep consistent of each hint message

--- azure-auth-helper/src/main/java/com/microsoft/azure/auth/configuration/AuthMethod.java
+++ azure-auth-helper/src/main/java/com/microsoft/azure/auth/configuration/AuthMethod.java
@@ -10,10 +10,20 @@ package com.microsoft.azure.auth.configuration;
  * Enum records detail auth method
  */
 public enum AuthMethod {
-    AZURE_CLI,
-    DEVICE_LOGIN,
-    CLOUD_SHELL,
-    OAUTH,
-    AZURE_SECRET_FILE,
-    SERVICE_PRINCIPAL
+    AZURE_CLI(AuthType.AZURE_CLI),
+    AZURE_SECRET_FILE(AuthType.AZURE_AUTH_MAVEN_PLUGIN),
+    CLOUD_SHELL(AuthType.AZURE_CLI),
+    DEVICE_LOGIN(AuthType.AZURE_AUTH_MAVEN_PLUGIN),
+    OAUTH(AuthType.AZURE_AUTH_MAVEN_PLUGIN),
+    SERVICE_PRINCIPAL(AuthType.SERVICE_PRINCIPAL);
+
+    private AuthType authType;
+
+    AuthMethod(AuthType authType) {
+        this.authType = authType;
+    }
+
+    public AuthType getAuthType() {
+        return authType;
+    }
 }
