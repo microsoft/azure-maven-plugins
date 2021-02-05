a21c3eefa8480c03222dd6fb5caa027a51414b6e
Mingliang Wang <wangmi@microsoft.com>
2021-02-05 16:15:22 +0800

    move `lib.common.appservice.file` from `azuretools-explorer-common` to `azure-toolkit-appservice-lib`

---
+++ azure-toolkit-libs/azure-toolkit-appservice-lib/src/main/java/com/microsoft/azure/toolkit/lib/appservice/file/AppServiceFile.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.microsoft.azure.toolkit.lib.appservice.file;
+
+import com.microsoft.azure.management.appservice.WebAppBase;
+import lombok.Data;
+
+import java.util.Objects;
+
+@Data
+public class AppServiceFile {
+    private String name;
+    private long size;
+    private String mtime;
+    private String crtime;
+    private String mime;
+    private String href;
+    private String path;
+    private WebAppBase app;
+
+    public String getId() {
+        return String.format("<%s>/%s", this.getApp().id(), this.getPath());
+    }
+
+    public String getFullName() {
+        return String.format("<%s>/%s", this.getApp().name(), this.getName());
+    }
+
+    public Type getType() {
+        return Objects.equals("inode/directory", this.mime) ? Type.DIRECTORY : Type.FILE;
+    }
+
+    public enum Type {
+        DIRECTORY, FILE
+    }
+}
---
+++ azure-toolkit-libs/azure-toolkit-appservice-lib/src/main/java/com/microsoft/azure/toolkit/lib/appservice/file/KuduFileClient.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.microsoft.azure.toolkit.lib.appservice.file;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Streaming;
+import rx.Observable;
+
+import java.util.List;
+
+public interface KuduFileClient extends AppServiceFileClient {
+    @Headers({
+                 "Content-Type: application/json; charset=utf-8",
+                 "x-ms-logging-context: com.microsoft.azure.management.appservice.WebApps getFile"
+             })
+    @GET("api/vfs/{path}")
+    @Streaming
+    Observable<ResponseBody> getFileContent(@Path("path") String path);
+
+    @Headers({
+                 "Content-Type: application/json; charset=utf-8",
+                 "x-ms-logging-context: com.microsoft.azure.management.appservice.WebApps getFilesInDirectory"
+             })
+    @GET("api/vfs/{path}/")
+    Observable<List<AppServiceFile>> getFilesInDirectory(@Path("path") String path);
+
+    @Headers({
+            "Content-Type: application/json; charset=utf-8",
+            "x-ms-logging-context: com.microsoft.azure.management.appservice.WebApps saveFile",
+            "If-Match: *"
+    })
+    @PUT("api/vfs/{path}")
+    Observable<Void> saveFile(@Path("path") String path, @Body RequestBody requestBody);
+
+    @Headers({
+                 "Content-Type: application/json; charset=utf-8",
+                 "x-ms-logging-context: com.microsoft.azure.management.appservice.WebApps createDirectory"
+             })
+    @PUT("api/vfs/{path}/")
+    Observable<ResponseBody> createDirectory(@Path("path") String path);
+
+    @Headers({
+                 "Content-Type: application/json; charset=utf-8",
+                 "x-ms-logging-context: com.microsoft.azure.management.appservice.WebApps deleteFile"
+             })
+    @DELETE("api/vfs/{path}")
+    Observable<ResponseBody> deleteFile(@Path("path") String path);
+}
