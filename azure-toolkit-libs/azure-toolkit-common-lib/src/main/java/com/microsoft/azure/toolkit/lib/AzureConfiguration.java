133e2cf800cbeefe6b025dd423ee5e623a426029
Mingliang Wang <wangmi@microsoft.com>
2021-02-08 15:26:21 +0800

    use `SPI` to manage azure services flexibly and achieve low coupling

---
+++ azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/AzureConfiguration.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.microsoft.azure.toolkit.lib;
+
+import com.microsoft.rest.LogLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AzureConfiguration {
+    private LogLevel logLevel;
+    private String userAgent;
+}
