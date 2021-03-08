8472e764b80d533b2d18df234f23305c27bf0d73
Mingliang Wang <wangmi@microsoft.com>
2021-03-08 23:23:58 +0800

    move AzureExceptionHandler and RestExceptionHandlerInterceptor to package `common.exception`

--- azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/exception/RestExceptionHandlerInterceptor.java
+++ azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/exception/RestExceptionHandlerInterceptor.java
@@ -3,7 +3,7 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
-package com.microsoft.azure.toolkit.lib.common.rest;
+package com.microsoft.azure.toolkit.lib.common.exception;
 
 import com.google.common.base.Throwables;
 import com.microsoft.aad.adal4j.AuthenticationException;
