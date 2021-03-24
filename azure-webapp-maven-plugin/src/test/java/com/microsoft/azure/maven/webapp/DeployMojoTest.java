51e692f2541003eb5b1702f1162c0c7eec584436
Hanxiao Liu <hanli@microsoft.com>
2021-03-24 13:03:48 +0800

    Add missing telemetries properties for Spring Cloud and WebApp maven plugin

--- azure-webapp-maven-plugin/src/test/java/com/microsoft/azure/maven/webapp/DeployMojoTest.java
+++ azure-webapp-maven-plugin/src/test/java/com/microsoft/azure/maven/webapp/DeployMojoTest.java
@@ -32,6 +32,7 @@ import java.util.Collections;
 import java.util.Map;
 
 import static com.microsoft.azure.maven.webapp.AbstractWebAppMojo.DEPLOYMENT_TYPE_KEY;
+import static com.microsoft.azure.maven.webapp.AbstractWebAppMojo.DEPLOY_TO_SLOT_KEY;
 import static com.microsoft.azure.maven.webapp.AbstractWebAppMojo.DOCKER_IMAGE_TYPE_KEY;
 import static com.microsoft.azure.maven.webapp.AbstractWebAppMojo.JAVA_VERSION_KEY;
 import static com.microsoft.azure.maven.webapp.AbstractWebAppMojo.JAVA_WEB_CONTAINER_KEY;
@@ -128,12 +129,13 @@ public class DeployMojoTest {
 
         final Map map = spyMojo.getTelemetryProperties();
 
-        assertEquals(11, map.size());
+        assertEquals(12, map.size());
         assertTrue(map.containsKey(JAVA_VERSION_KEY));
         assertTrue(map.containsKey(JAVA_WEB_CONTAINER_KEY));
         assertTrue(map.containsKey(DOCKER_IMAGE_TYPE_KEY));
         assertTrue(map.containsKey(DEPLOYMENT_TYPE_KEY));
         assertTrue(map.containsKey(OS_KEY));
+        assertTrue(map.containsKey(DEPLOY_TO_SLOT_KEY));
     }
 
     @Test
