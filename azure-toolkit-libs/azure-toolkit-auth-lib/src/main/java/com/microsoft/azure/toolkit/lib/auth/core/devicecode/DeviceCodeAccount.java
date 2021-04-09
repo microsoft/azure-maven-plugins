8d4a85c865668b9f0f44e4212b545d670e788943
andxu <andxu@microsoft.com>
2021-04-09 10:28:06 +0800

    add device code login

---
+++ azure-toolkit-libs/azure-toolkit-auth-lib/src/main/java/com/microsoft/azure/toolkit/lib/auth/core/devicecode/DeviceCodeAccount.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.microsoft.azure.toolkit.lib.auth.core.devicecode;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.identity.DeviceCodeCredentialBuilder;
+import com.azure.identity.DeviceCodeInfo;
+import com.azure.identity.implementation.util.IdentityConstants;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.microsoft.azure.toolkit.lib.auth.Account;
+import com.microsoft.azure.toolkit.lib.auth.exception.AzureToolkitAuthenticationException;
+import com.microsoft.azure.toolkit.lib.auth.model.AuthMethod;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+public class DeviceCodeAccount extends Account {
+    private static final ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+            .setNameFormat("azure-toolkit-auth-%d").build();
+
+    private final ExecutorService executorService = createExecutorService();
+
+    private final CompletableFuture<DeviceCodeInfo> deviceCodeFuture = new CompletableFuture<>();
+
+    private Mono<Account> loginMono;
+
+    @Override
+    public AuthMethod getMethod() {
+        return AuthMethod.DEVICE_CODE;
+    }
+
+    @Override
+    protected boolean checkAvailableInner() {
+        return true;
+    }
+
+    @Override
+    protected TokenCredential createTokenCredential() {
+        if (executorService.isShutdown()) {
+            throw new AzureToolkitAuthenticationException("Cannot device login twice.");
+        }
+        DeviceCodeCredentialBuilder builder = new DeviceCodeCredentialBuilder();
+        return builder.clientId(IdentityConstants.DEVELOPER_SINGLE_SIGN_ON_ID)
+                .executorService(executorService)
+                .challengeConsumer(deviceCodeFuture::complete).build();
+    }
+
+    @Override
+    protected Mono<Account> authenticate() {
+        Mono<Account> map = Mono.fromFuture(deviceCodeFuture).map(ignore -> this);
+        loginMono = super.authenticate().doFinally(r -> executorService.shutdown()
+        ).subscribeOn(Schedulers.boundedElastic()).cache();
+        return map.doOnCancel(executorService::shutdownNow).doOnSubscribe(ignore -> loginMono.subscribe());
+    }
+
+    public Mono<Account> continueLogin() {
+        return loginMono;
+    }
+
+    public DeviceCodeInfo getDeviceCode() {
+        try {
+            return this.deviceCodeFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new AzureToolkitAuthenticationException(e.getMessage());
+        }
+    }
+
+    private static ExecutorService createExecutorService() {
+        return Executors.newFixedThreadPool(2, namedThreadFactory);
+    }
+}
