2d9b06cf09032e525987b67c167a06ac56d29598
andxu <andxu@microsoft.com>
2021-04-18 18:44:08 +0800

    remove the heavy api call :list tenants on check avail and remove authenticated

--- azure-toolkit-libs/azure-toolkit-auth-lib/src/main/java/com/microsoft/azure/toolkit/lib/auth/AzureAccount.java
+++ azure-toolkit-libs/azure-toolkit-auth-lib/src/main/java/com/microsoft/azure/toolkit/lib/auth/AzureAccount.java
@@ -46,10 +46,6 @@ public class AzureAccount implements AzureService, IAzureAccount {
     @Setter(AccessLevel.PRIVATE)
     private Account account;
 
-    @Setter
-    @Getter
-    private boolean enablePersistence = false;
-
     /**
      * @return the current account
      * @throws AzureToolkitAuthenticationException if not initialized
@@ -174,7 +170,7 @@ public class AzureAccount implements AzureService, IAzureAccount {
         }
     }
 
-    public Mono<Account> loginAsync(AuthType type, boolean enablePersistence) {
+    public Mono<Account> loginAsync(@Nonnull AuthType type, boolean enablePersistence) {
         Objects.requireNonNull(type, "Please specify auth type in auth configuration.");
         AuthConfiguration auth = new AuthConfiguration();
         auth.setType(type);
@@ -218,7 +214,7 @@ public class AzureAccount implements AzureService, IAzureAccount {
     }
 
     private static void checkEnv(Account ac, AzureEnvironment env) {
-        if (env != null && ac.getEnvironment() != null && ac.getEnvironment() != env && ac.isAvailable()) {
+        if (env != null && ac.getEnvironment() != null && ac.getEnvironment() != env) {
             String expectedEnv = AzureEnvironmentUtils.getCloudNameForAzureCli(env);
             String realEnv = AzureEnvironmentUtils.getCloudNameForAzureCli(ac.getEnvironment());
 
