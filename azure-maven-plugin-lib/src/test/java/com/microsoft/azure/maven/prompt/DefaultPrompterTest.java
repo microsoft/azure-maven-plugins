383fb74b6872ef30063cf36bed6117fbf2fd51a0
Wang Mingliang <wangmi@microsoft.com>
2021-05-02 17:19:43 +0800

    fix checkstyle

--- azure-maven-plugin-lib/src/test/java/com/microsoft/azure/maven/prompt/DefaultPrompterTest.java
+++ azure-maven-plugin-lib/src/test/java/com/microsoft/azure/maven/prompt/DefaultPrompterTest.java
@@ -13,6 +13,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.BufferedReader;
@@ -22,12 +23,9 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-
 @RunWith(MockitoJUnitRunner.class)
 public class DefaultPrompterTest {
-    private final BufferedReader reader = mock(BufferedReader.class);
+    private final BufferedReader reader = Mockito.mock(BufferedReader.class);
     private final DefaultPrompter prompter = new DefaultPrompter();
 
     @Before
@@ -37,12 +35,12 @@ public class DefaultPrompterTest {
 
     @Test
     public void testPromoteStringRequired() throws Exception {
-        when(reader.readLine()).thenReturn("bar");
+        Mockito.when(reader.readLine()).thenReturn("bar");
         String result = prompter.promoteString("Please input a string", "foo", input -> {
             if (StringUtils.equals("bar", input)) {
                 // cannot input bar, set 10 to next call;
                 try {
-                    when(reader.readLine()).thenReturn(" 10 ");
+                    Mockito.when(reader.readLine()).thenReturn(" 10 ");
                 } catch (IOException e) {
                     throw Lombok.sneakyThrow(e);
                 }
@@ -50,79 +48,79 @@ public class DefaultPrompterTest {
             }
             return InputValidateResult.wrap(input.trim());
         }, true);
-        assertEquals("10", result);
+        Assert.assertEquals("10", result);
 
-        when(reader.readLine()).thenReturn("");
+        Mockito.when(reader.readLine()).thenReturn("");
 
         result = prompter.promoteString("Please input a string", "foo", input -> {
             throw new RuntimeException();
         }, true);
-        assertEquals("foo", result);
+        Assert.assertEquals("foo", result);
 
-        when(reader.readLine()).thenReturn("").thenReturn("").thenReturn("a");
+        Mockito.when(reader.readLine()).thenReturn("").thenReturn("").thenReturn("a");
         result = prompter.promoteString("Please input a string", null, InputValidateResult::wrap, true);
-        assertEquals("a", result);
+        Assert.assertEquals("a", result);
     }
 
     @Test
     public void testPromoteStringNotRequired() throws Exception {
-        when(reader.readLine()).thenReturn("");
+        Mockito.when(reader.readLine()).thenReturn("");
         String result = prompter.promoteString("Please input a string", "foo", input -> {
             throw new RuntimeException();
         }, false);
-        assertEquals("foo", result);
+        Assert.assertEquals("foo", result);
 
         result = prompter.promoteString("Please input a string", null, input -> {
             throw new RuntimeException();
         }, false);
-        assertNull(result);
+        Assert.assertNull(result);
     }
 
     @Test
     public void testPromoteStringEmpty() throws Exception {
-        when(reader.readLine()).thenReturn(":");
+        Mockito.when(reader.readLine()).thenReturn(":");
         final String result = prompter.promoteString("Please input a string", "foo", input -> {
             throw new RuntimeException();
         }, false);
-        assertEquals("", result);
+        Assert.assertEquals("", result);
     }
 
     @Test
     public void testPromoteYesNo() throws Exception {
-        when(reader.readLine()).thenReturn("Y").thenReturn("y").thenReturn("n");
+        Mockito.when(reader.readLine()).thenReturn("Y").thenReturn("y").thenReturn("n");
         Boolean result = prompter.promoteYesNo("Do you want to continue(y/n)", null, true);
-        assertNotNull(result);
-        assertTrue(result);
+        Assert.assertNotNull(result);
+        Assert.assertTrue(result);
 
         result = prompter.promoteYesNo("Do you want to continue(y/n)", null, true);
-        assertNotNull(result);
-        assertTrue(result);
+        Assert.assertNotNull(result);
+        Assert.assertTrue(result);
 
         result = prompter.promoteYesNo("Do you want to continue(y/n)", null, true);
-        assertNotNull(result);
-        assertFalse(result);
+        Assert.assertNotNull(result);
+        Assert.assertFalse(result);
 
-        when(reader.readLine()).thenReturn("").thenReturn("");
+        Mockito.when(reader.readLine()).thenReturn("").thenReturn("");
         result = prompter.promoteYesNo("Do you want to continue(Y/n)", true, false);
-        assertNotNull(result);
-        assertTrue(result);
+        Assert.assertNotNull(result);
+        Assert.assertTrue(result);
 
         result = prompter.promoteYesNo("Do you want to continue(Y/n)", false, false);
-        assertNotNull(result);
-        assertFalse(result);
+        Assert.assertNotNull(result);
+        Assert.assertFalse(result);
     }
 
     @Test
     public void testPromoteYesNoBadInput() throws Exception {
-        when(reader.readLine()).thenReturn("foo").thenReturn("bar").thenReturn("Y");
+        Mockito.when(reader.readLine()).thenReturn("foo").thenReturn("bar").thenReturn("Y");
         final Boolean result = prompter.promoteYesNo("Do you want to continue(Y/n)", null, true);
-        assertNotNull(result);
-        assertTrue(result);
+        Assert.assertNotNull(result);
+        Assert.assertTrue(result);
     }
 
     @Test
     public void testPromoteMultipleEntities() throws Exception {
-        when(reader.readLine()).thenReturn("1").thenReturn("1-2").thenReturn("1-2,3-5").thenReturn("3-1000000,1-2,3-5");
+        Mockito.when(reader.readLine()).thenReturn("1").thenReturn("1-2").thenReturn("1-2,3-5").thenReturn("3-1000000,1-2,3-5");
         final List<Integer> integers = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             integers.add(i);
@@ -146,19 +144,19 @@ public class DefaultPrompterTest {
                 "to select none", Collections.emptyList());
         Assert.assertEquals("2,3,4,5,6,7,8,9,0,1", TestHelper.joinIntegers(selected));
 
-        when(reader.readLine()).thenReturn("bad value").thenReturn("100").thenReturn("");
+        Mockito.when(reader.readLine()).thenReturn("bad value").thenReturn("100").thenReturn("");
         selected = prompter.promoteMultipleEntities("This is header", "Please input range",
                 "You have select no entities", integers, t -> t.toString(), true,
                 "to select none", Collections.emptyList());
-        assertTrue(selected.isEmpty());
+        Assert.assertTrue(selected.isEmpty());
 
-        when(reader.readLine()).thenReturn("");
+        Mockito.when(reader.readLine()).thenReturn("");
         selected = prompter.promoteMultipleEntities("This is header", "Please input range",
                 "You have select no entities", Collections.singletonList(1), t -> t.toString(), true,
                 "to select none", Collections.emptyList());
-        assertTrue(selected.isEmpty());
+        Assert.assertTrue(selected.isEmpty());
 
-        when(reader.readLine()).thenReturn("");
+        Mockito.when(reader.readLine()).thenReturn("");
         selected = prompter.promoteMultipleEntities("This is header", "Please input range",
         "You have select no entities", integers, t -> t.toString(), true,
         "to select none", Arrays.asList(4, 5));
@@ -167,7 +165,7 @@ public class DefaultPrompterTest {
 
     @Test
     public void testPromoteMultipleEntitiesAllowEmpty() throws Exception {
-        when(reader.readLine()).thenReturn("1").thenReturn("1-2").thenReturn("1-2,3-5").thenReturn("3-1000000,1-2,3-5");
+        Mockito.when(reader.readLine()).thenReturn("1").thenReturn("1-2").thenReturn("1-2,3-5").thenReturn("3-1000000,1-2,3-5");
         final List<Integer> integers = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             integers.add(i);
@@ -191,19 +189,19 @@ public class DefaultPrompterTest {
                 "to select none", Collections.emptyList());
         Assert.assertEquals("2,3,4,5,6,7,8,9,0,1", TestHelper.joinIntegers(selected));
 
-        when(reader.readLine()).thenReturn("bad value").thenReturn("100").thenReturn("");
+        Mockito.when(reader.readLine()).thenReturn("bad value").thenReturn("100").thenReturn("");
         selected = prompter.promoteMultipleEntities("This is header", "Please input range",
                 "You have select no entities", integers, t -> t.toString(), true,
                 "to select none", Collections.emptyList());
-        assertTrue(selected.isEmpty());
+        Assert.assertTrue(selected.isEmpty());
 
-        when(reader.readLine()).thenReturn("");
+        Mockito.when(reader.readLine()).thenReturn("");
         selected = prompter.promoteMultipleEntities("This is header", "Please input range",
                 "You have select no entities", Collections.singletonList(1), t -> t.toString(), true,
                 "to select none", Collections.emptyList());
-        assertTrue(selected.isEmpty());
+        Assert.assertTrue(selected.isEmpty());
 
-        when(reader.readLine()).thenReturn("");
+        Mockito.when(reader.readLine()).thenReturn("");
         selected = prompter.promoteMultipleEntities("This is header", "Please input range",
         "You have select no entities", integers, t -> t.toString(), true,
         "to select none", Arrays.asList(4, 5));
@@ -220,7 +218,7 @@ public class DefaultPrompterTest {
 
     @Test
     public void testPromoteMultipleEntitiesNotAllowEmptyNoDefaultValue() throws Exception {
-        when(reader.readLine()).thenReturn("1000-11111").thenReturn("10001111111111111111111111111").thenReturn("2");;
+        Mockito.when(reader.readLine()).thenReturn("1000-11111").thenReturn("10001111111111111111111111111").thenReturn("2");;
         final List<Integer> selected = prompter.promoteMultipleEntities("This is header", "Please input range",
                 "You have select no entities", Arrays.asList(98, 99, 100), t -> t.toString(), false,
                 "to select none", Collections.emptyList());
@@ -229,13 +227,13 @@ public class DefaultPrompterTest {
 
     @Test
     public void testPromoteMultipleEntitiesSelectNone() throws Exception {
-        when(reader.readLine()).thenReturn("");
+        Mockito.when(reader.readLine()).thenReturn("");
         List<Integer> selected = prompter.promoteMultipleEntities("This is header", "Please input range",
                 "You have select no entities", Collections.singletonList(100), t -> t.toString(), true,
                 "to select none", Collections.emptyList());
-        assertEquals(0, selected.size());
+        Assert.assertEquals(0, selected.size());
 
-        when(reader.readLine()).thenReturn("100000000000000").thenReturn("1");
+        Mockito.when(reader.readLine()).thenReturn("100000000000000").thenReturn("1");
         selected = prompter.promoteMultipleEntities("This is header", "Please input range",
                 "You have select no entities", Collections.singletonList(100), t -> t.toString(), true,
                 "to select none", Collections.emptyList());
@@ -244,25 +242,25 @@ public class DefaultPrompterTest {
 
     @Test
     public void testPromoteSingle() throws Exception {
-        when(reader.readLine()).thenReturn("1").thenReturn("10001111111111111111111111111").thenReturn("1000").thenReturn("2").thenReturn("");
+        Mockito.when(reader.readLine()).thenReturn("1").thenReturn("10001111111111111111111111111").thenReturn("1000").thenReturn("2").thenReturn("");
         final List<Integer> integers = new ArrayList<>();
         for (int i = 1; i <= 3; i++) {
             integers.add(i);
         }
         Integer value = prompter.promoteSingleEntity("This is header", "Please input index for integer", integers, 2, t -> t.toString(), false);
-        assertEquals(Integer.valueOf(1), value);
+        Assert.assertEquals(Integer.valueOf(1), value);
         value = prompter.promoteSingleEntity("This is header", "Please input index for integer", integers, 2, t -> t.toString(), false);
-        assertEquals(Integer.valueOf(2), value);
+        Assert.assertEquals(Integer.valueOf(2), value);
         value = prompter.promoteSingleEntity("This is header", "Please input index for integer", integers, 2, t -> t.toString(), false);
-        assertEquals(Integer.valueOf(2), value);
-        when(reader.readLine()).thenReturn("bad number").thenReturn("10").thenReturn("3");
+        Assert.assertEquals(Integer.valueOf(2), value);
+        Mockito.when(reader.readLine()).thenReturn("bad number").thenReturn("10").thenReturn("3");
         value = prompter.promoteSingleEntity("This is header", "Please input index for integer", integers, 2, t -> t.toString(), false);
-        assertEquals(Integer.valueOf(3), value);
+        Assert.assertEquals(Integer.valueOf(3), value);
     }
 
     @Test
     public void testClose() throws Exception {
-        doNothing().when(reader).close();
+        Mockito.doNothing().when(reader).close();
         this.prompter.close();
     }
 }
