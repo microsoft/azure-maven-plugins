821a036f862358cf6a948efc43c1cbe5842ee455
Hanxiao Liu <hanli@microsoft.com>
2021-05-14 10:18:42 +0800

    Refine application insights entity

--- azure-toolkit-libs/azure-toolkit-applicationinsights-lib/src/main/java/com/microsoft/azure/toolkit/lib/applicationinsights/ApplicationInsightsEntity.java
+++ azure-toolkit-libs/azure-toolkit-applicationinsights-lib/src/main/java/com/microsoft/azure/toolkit/lib/applicationinsights/ApplicationInsightsEntity.java
@@ -1,17 +1,19 @@
 package com.microsoft.azure.toolkit.lib.applicationinsights;
 
+import com.microsoft.azure.toolkit.lib.common.entity.IAzureResourceEntity;
+import com.microsoft.azure.toolkit.lib.common.model.Region;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder(toBuilder = true)
-public class ApplicationInsightsEntity {
+public class ApplicationInsightsEntity implements IAzureResourceEntity {
+    private String id;
+    private String name;
     private String subscriptionId;
-    private String resourceId;
-    private String resourceName;
+    private String resourceGroup;
     private String instrumentationKey;
     private String type;
     private String kind;
-    private String location;
-    private String resourceGroup;
+    private Region region;
 }
