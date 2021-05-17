636b57b549000eca77fb8761063ad4b529fb6ac5
Hanxiao Liu <hanli@microsoft.com>
2021-05-17 17:32:23 +0800

    Implement deploy handler for function apps

--- azure-toolkit-libs/azure-toolkit-appservice-lib/src/main/java/com/microsoft/azure/toolkit/lib/appservice/service/impl/deploy/MSFunctionDeployHandler.java
+++ azure-toolkit-libs/azure-toolkit-appservice-lib/src/main/java/com/microsoft/azure/toolkit/lib/appservice/service/impl/deploy/MSFunctionDeployHandler.java
@@ -4,13 +4,80 @@
  */
 package com.microsoft.azure.toolkit.lib.appservice.service.impl.deploy;
 
+import com.azure.core.util.logging.ClientLogger;
 import com.azure.resourcemanager.appservice.models.WebAppBase;
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.blob.BlobContainerPublicAccessType;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import com.microsoft.azure.toolkit.lib.common.exception.AzureExecutionException;
+import com.microsoft.azure.toolkit.lib.common.exception.AzureToolkitRuntimeException;
+import com.microsoft.azure.toolkit.lib.legacy.function.AzureStorageHelper;
+import com.microsoft.azure.toolkit.lib.legacy.function.Constants;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.time.Period;
+import java.util.Date;
 
 public class MSFunctionDeployHandler implements IFunctionDeployHandler {
-    @Override
-    public void deploy(File file, WebAppBase functionApp) {
+    private static final ClientLogger LOGGER = new ClientLogger(MSFunctionDeployHandler.class);
+    private static final String DEPLOYMENT_PACKAGE_CONTAINER = "java-functions-deployment-packages";
+    private static final String UPLOAD_PACKAGE_START = "Step 1 of 3: Uploading ZIP file to Azure Storage...";
+    private static final String UPLOAD_PACKAGE_DONE = "Successfully uploaded ZIP file to ";
+    private static final String DEPLOY_PACKAGE_START = "Step 2 of 3: Deploying Azure Function App with package...";
+    private static final String DEPLOY_PACKAGE_DONE = "Successfully deployed Azure Function App.";
+    private static final String DELETE_PACKAGE_START = "Step 3 of 3: Deleting deployment package from Azure Storage...";
+    private static final String DELETE_PACKAGE_DONE = "Successfully deleted deployment package ";
+    private static final String DELETE_PACKAGE_FAIL = "Failed to delete deployment package ";
 
+    @Override
+    public void deploy(final File file, final WebAppBase webAppBase) {
+        final CloudStorageAccount storageAccount = DeployUtils.getCloudStorageAccount(webAppBase);
+
+        final String blobName = getBlobName(webAppBase);
+
+        final String packageUri = uploadPackageToAzureStorage(file, storageAccount, blobName);
+
+        deployWithPackageUri(webAppBase, packageUri, () -> deletePackageFromAzureStorage(storageAccount, blobName));
+    }
+
+    protected String getBlobName(final WebAppBase functionApp) {
+        return functionApp.name()
+                .concat(new SimpleDateFormat(".yyyyMMddHHmmssSSS").format(new Date()))
+                .concat(Constants.ZIP_EXT);
+    }
+
+    protected String uploadPackageToAzureStorage(final File zipPackage, final CloudStorageAccount storageAccount,
+                                                 final String blobName) {
+        LOGGER.info(UPLOAD_PACKAGE_START);
+        try {
+            final CloudBlockBlob blob = AzureStorageHelper.uploadFileAsBlob(zipPackage, storageAccount,
+                    DEPLOYMENT_PACKAGE_CONTAINER, blobName, BlobContainerPublicAccessType.OFF);
+            final String packageUri = AzureStorageHelper.getSASToken(blob, Period.ofDays(1)); // no need for a long period as it will be deleted after deployment
+            LOGGER.info(UPLOAD_PACKAGE_DONE + blob.getUri().toString());
+            return packageUri;
+        } catch (AzureExecutionException e) {
+            throw new AzureToolkitRuntimeException("Failed to update package to azure storage", e);
+        }
+    }
+
+    protected void deployWithPackageUri(final WebAppBase target, final String packageUri, Runnable onDeployFinish) {
+        try {
+            LOGGER.info(DEPLOY_PACKAGE_START);
+            target.deploy().withPackageUri(packageUri).execute().complete();
+            LOGGER.info(DEPLOY_PACKAGE_DONE);
+        } finally {
+            onDeployFinish.run();
+        }
+    }
+
+    protected void deletePackageFromAzureStorage(final CloudStorageAccount storageAccount, final String blobName) {
+        try {
+            LOGGER.info(DELETE_PACKAGE_START);
+            AzureStorageHelper.deleteBlob(storageAccount, DEPLOYMENT_PACKAGE_CONTAINER, blobName);
+            LOGGER.info(DELETE_PACKAGE_DONE + blobName);
+        } catch (Exception e) {
+            LOGGER.error(DELETE_PACKAGE_FAIL + blobName);
+        }
     }
 }
--- azure-toolkit-libs/azure-toolkit-appservice-lib/src/main/java/com/microsoft/azure/toolkit/lib/appservice/service/impl/deploy/RunFromZipFunctionDeployHandler.java
+++ azure-toolkit-libs/azure-toolkit-appservice-lib/src/main/java/com/microsoft/azure/toolkit/lib/appservice/service/impl/deploy/RunFromZipFunctionDeployHandler.java
@@ -8,9 +8,19 @@ import com.azure.resourcemanager.appservice.models.WebAppBase;
 
 import java.io.File;
 
-public class RunFromZipFunctionDeployHandler  implements IFunctionDeployHandler{
+public class RunFromZipFunctionDeployHandler extends ZIPFunctionDeployHandler {
+    private static final String APP_SETTING_WEBSITE_RUN_FROM_PACKAGE = "WEBSITE_RUN_FROM_PACKAGE";
+    private static final String RUN_FROM_PACKAGE_VALUE = "1";
+
     @Override
     public void deploy(File file, WebAppBase functionApp) {
-
+        DeployUtils.updateFunctionAppSetting(functionApp, APP_SETTING_WEBSITE_RUN_FROM_PACKAGE, RUN_FROM_PACKAGE_VALUE);
+        try {
+            // work around for issue https://dev.azure.com/msazure/Unified%20Platform%20KPIs/_workitems/edit/7481871
+            Thread.sleep(20 * 1000);
+        } catch (InterruptedException e) {
+            // Sorrow exception here as we just wait for 20s for kudu app settings update, which is not required in normal cases
+        }
+        super.deploy(file, functionApp);
     }
 }
