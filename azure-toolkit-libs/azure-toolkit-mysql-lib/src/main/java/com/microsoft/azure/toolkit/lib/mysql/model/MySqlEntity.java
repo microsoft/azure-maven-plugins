a7ac4e8db8cf1b8fc233e75f916ab3bc3106da49
andxu <andxu@microsoft.com>
2021-06-18 10:38:38 +0800

    [Task 1847361]3. add main logics to mysql-lib(start, stop...)

--- azure-toolkit-libs/azure-toolkit-mysql-lib/src/main/java/com/microsoft/azure/toolkit/lib/mysql/model/MySqlEntity.java
+++ azure-toolkit-libs/azure-toolkit-mysql-lib/src/main/java/com/microsoft/azure/toolkit/lib/mysql/model/MySqlEntity.java
@@ -5,31 +5,86 @@
 
 package com.microsoft.azure.toolkit.lib.mysql.model;
 
-import com.microsoft.azure.toolkit.lib.common.entity.IAzureResourceEntity;
+import com.azure.core.util.ExpandableStringEnum;
+import com.azure.resourcemanager.mysql.models.Server;
+import com.azure.resourcemanager.mysql.models.Sku;
+import com.azure.resourcemanager.mysql.models.SslEnforcementEnum;
+import com.azure.resourcemanager.mysql.models.StorageProfile;
+import com.azure.resourcemanager.resources.fluentcore.arm.ResourceId;
 import com.microsoft.azure.toolkit.lib.common.model.Region;
-import lombok.Getter;
-import lombok.experimental.SuperBuilder;
+import com.microsoft.azure.toolkit.lib.mysql.service.AbstractAzureEntityManager;
 
-@Getter
-@SuperBuilder(toBuilder = true)
-public class MySqlEntity implements IAzureResourceEntity {
-    private String id;
-    private String subscriptionId;
-    private String resourceGroup;
-    private String name;
-    private Region region;
+import javax.annotation.Nonnull;
+import java.util.Optional;
 
-    /**
-     * TODO(qianjin): for type, state and kind, give the possible values in javadoc
-     */
-    private String administratorLoginName;
-    private String version;
-    private String state;
-    private String fullyQualifiedDomainName;
-    private String type;
+public class MySqlEntity extends AbstractAzureEntityManager.RemoteAwareResourceEntity<Server> {
 
-    private String skuTier;
-    private int vCore;
-    private int storageInMB;
-    private String sslEnforceStatus;
+    public MySqlEntity(Server server) {
+        this.resourceId = ResourceId.fromString(server.id());
+        this.remote = server;
+    }
+
+    private @Nonnull ResourceId resourceId;
+
+    public String getId() {
+        return resourceId.id();
+    }
+
+    public String getResourceGroup() {
+        return resourceId.resourceGroupName();
+    }
+
+    @Override
+    public String getName() {
+        return resourceId.name();
+    }
+
+    @Override
+    public String getSubscriptionId() {
+        return resourceId.subscriptionId();
+    }
+
+    private Optional<Server> remoteOptional() {
+        return Optional.ofNullable(this.remote);
+    }
+
+    public Region getRegion() {
+        return remoteOptional().map(remote -> Region.fromName(remote.regionName())).orElse(null);
+    }
+
+    public Region getAdministratorLoginName() {
+        return remoteOptional().map(remote -> Region.fromName(remote.administratorLogin())).orElse(null);
+    }
+
+    public String getVersion() {
+        return remoteOptional().map(Server::version).map(ExpandableStringEnum::toString).orElse(null);
+    }
+
+    public String getState() {
+        return remoteOptional().map(Server::userVisibleState).map(ExpandableStringEnum::toString).orElse(null);
+    }
+
+    public String getFullyQualifiedDomainName() {
+        return remoteOptional().map(Server::fullyQualifiedDomainName).orElse(null);
+    }
+
+    public String getType() {
+        return remoteOptional().map(Server::type).orElse(null);
+    }
+
+    public String getSkuTier() {
+        return remoteOptional().map(Server::sku).map(Sku::tier).map(ExpandableStringEnum::toString).orElse(null);
+    }
+
+    public int getVCore() {
+        return remoteOptional().map(Server::sku).map(Sku::capacity).orElse(0);
+    }
+
+    public int getStorageInMB() {
+        return remoteOptional().map(Server::storageProfile).map(StorageProfile::storageMB).orElse(0);
+    }
+
+    public String getSslEnforceStatus() {
+        return remoteOptional().map(Server::sslEnforcement).map(SslEnforcementEnum::name).orElse(null);
+    }
 }
