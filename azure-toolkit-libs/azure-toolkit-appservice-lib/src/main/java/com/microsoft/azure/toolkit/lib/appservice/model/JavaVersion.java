486c131f7b1456b0cc0b0fc12138db72a29570e7
Hanxiao Liu <hanli@microsoft.com>
2021-07-12 13:53:21 +0800

    Remove public access for java version with minor version

--- azure-toolkit-libs/azure-toolkit-appservice-lib/src/main/java/com/microsoft/azure/toolkit/lib/appservice/model/JavaVersion.java
+++ azure-toolkit-libs/azure-toolkit-appservice-lib/src/main/java/com/microsoft/azure/toolkit/lib/appservice/model/JavaVersion.java
@@ -36,24 +36,24 @@ public class JavaVersion implements ExpandableParameter {
     public static final JavaVersion JAVA_8 = new JavaVersion("1.8");
     public static final JavaVersion JAVA_11 = new JavaVersion("11");
 
-    public static final JavaVersion JAVA_1_7_0_51 = new JavaVersion("1.7.0_51");
-    public static final JavaVersion JAVA_1_7_0_71 = new JavaVersion("1.7.0_71");
-    public static final JavaVersion JAVA_1_7_0_80 = new JavaVersion("1.7.0_80");
-    public static final JavaVersion JAVA_ZULU_1_7_0_191 = new JavaVersion("1.7.0_191_ZULU");
-    public static final JavaVersion JAVA_1_8_0_25 = new JavaVersion("1.8.0_25");
-    public static final JavaVersion JAVA_1_8_0_60 = new JavaVersion("1.8.0_60");
-    public static final JavaVersion JAVA_1_8_0_73 = new JavaVersion("1.8.0_73");
-    public static final JavaVersion JAVA_1_8_0_111 = new JavaVersion("1.8.0_111");
-    public static final JavaVersion JAVA_1_8_0_144 = new JavaVersion("1.8.0_144");
-    public static final JavaVersion JAVA_1_8_0_172 = new JavaVersion("1.8.0_172");
-    public static final JavaVersion JAVA_ZULU_1_8_0_172 = new JavaVersion("1.8.0_172_ZULU");
-    public static final JavaVersion JAVA_ZULU_1_8_0_92 = new JavaVersion("1.8.0_92");
-    public static final JavaVersion JAVA_ZULU_1_8_0_102 = new JavaVersion("1.8.0_102");
-    public static final JavaVersion JAVA_1_8_0_181 = new JavaVersion("1.8.0_181");
-    public static final JavaVersion JAVA_ZULU_1_8_0_181 = new JavaVersion("1.8.0_181_ZULU");
-    public static final JavaVersion JAVA_1_8_0_202 = new JavaVersion("1.8.0_202");
-    public static final JavaVersion JAVA_ZULU_1_8_0_202 = new JavaVersion("1.8.0_202_ZULU");
-    public static final JavaVersion JAVA_ZULU_11_0_2 = new JavaVersion("11.0.2_ZULU");
+    private static final JavaVersion JAVA_1_7_0_51 = new JavaVersion("1.7.0_51");
+    private static final JavaVersion JAVA_1_7_0_71 = new JavaVersion("1.7.0_71");
+    private static final JavaVersion JAVA_1_7_0_80 = new JavaVersion("1.7.0_80");
+    private static final JavaVersion JAVA_ZULU_1_7_0_191 = new JavaVersion("1.7.0_191_ZULU");
+    private static final JavaVersion JAVA_1_8_0_25 = new JavaVersion("1.8.0_25");
+    private static final JavaVersion JAVA_1_8_0_60 = new JavaVersion("1.8.0_60");
+    private static final JavaVersion JAVA_1_8_0_73 = new JavaVersion("1.8.0_73");
+    private static final JavaVersion JAVA_1_8_0_111 = new JavaVersion("1.8.0_111");
+    private static final JavaVersion JAVA_1_8_0_144 = new JavaVersion("1.8.0_144");
+    private static final JavaVersion JAVA_1_8_0_172 = new JavaVersion("1.8.0_172");
+    private static final JavaVersion JAVA_ZULU_1_8_0_172 = new JavaVersion("1.8.0_172_ZULU");
+    private static final JavaVersion JAVA_ZULU_1_8_0_92 = new JavaVersion("1.8.0_92");
+    private static final JavaVersion JAVA_ZULU_1_8_0_102 = new JavaVersion("1.8.0_102");
+    private static final JavaVersion JAVA_1_8_0_181 = new JavaVersion("1.8.0_181");
+    private static final JavaVersion JAVA_ZULU_1_8_0_181 = new JavaVersion("1.8.0_181_ZULU");
+    private static final JavaVersion JAVA_1_8_0_202 = new JavaVersion("1.8.0_202");
+    private static final JavaVersion JAVA_ZULU_1_8_0_202 = new JavaVersion("1.8.0_202_ZULU");
+    private static final JavaVersion JAVA_ZULU_11_0_2 = new JavaVersion("11.0.2_ZULU");
 
     private static final Set<JavaVersion> values = Collections.unmodifiableSet(Sets.newHashSet(OFF, JAVA_7, JAVA_1_7_0_51, JAVA_1_7_0_71, JAVA_1_7_0_80,
             JAVA_ZULU_1_7_0_191, JAVA_8, JAVA_1_8_0_25, JAVA_1_8_0_60, JAVA_1_8_0_73, JAVA_1_8_0_111, JAVA_1_8_0_144, JAVA_1_8_0_172, JAVA_ZULU_1_8_0_172,
