aa49288737f9bf20daa520666851b4b3bbea1277
andxu <andxu@microsoft.com>
2021-07-15 09:20:42 +0800

    add proxy in azure cli commands to fix the bug of az doesn't work in proxy mode

--- azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/utils/CommandUtils.java
+++ azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/utils/CommandUtils.java
@@ -16,6 +16,8 @@ import org.apache.commons.lang3.SystemUtils;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * copied from: https://github.com/microsoft/azure-tools-for-java/blob/
@@ -31,6 +33,10 @@ public class CommandUtils {
     private static final String DEFAULT_MAC_LINUX_PATH = "/bin/";
 
     public static String exec(final String commandWithArgs) throws IOException {
+        return exec(commandWithArgs, new HashMap<>());
+    }
+
+    public static String exec(final String commandWithArgs, Map<String, String> env) throws IOException {
         final String starter = isWindows() ? WINDOWS_STARTER : LINUX_MAC_STARTER;
         final String switcher = isWindows() ? WINDOWS_SWITCHER : LINUX_MAC_SWITCHER;
         final String workingDirectory = getSafeWorkingDirectory();
@@ -40,18 +46,18 @@ public class CommandUtils {
             throw exception;
         }
         final String commandWithPath = isWindows() ? commandWithArgs : String.format("export PATH=$PATH:/usr/local/bin ; %s", commandWithArgs);
-        return executeCommandAndGetOutput(starter, switcher, commandWithPath, new File(workingDirectory));
+        return executeCommandAndGetOutput(starter, switcher, commandWithPath, new File(workingDirectory), env);
     }
 
     private static String executeCommandAndGetOutput(final String starter, final String switcher, final String commandWithArgs,
-                                                    final File directory) throws IOException {
+                                                    final File directory, Map<String, String> env) throws IOException {
         final CommandLine commandLine = new CommandLine(starter);
         commandLine.addArgument(switcher, false);
         commandLine.addArgument(commandWithArgs, false);
-        return executeCommandAndGetOutput(commandLine, directory);
+        return executeCommandAndGetOutput(commandLine, directory, env);
     }
 
-    private static String executeCommandAndGetOutput(final CommandLine commandLine, final File directory) throws IOException {
+    private static String executeCommandAndGetOutput(final CommandLine commandLine, final File directory, Map<String, String> env) throws IOException {
         final ByteArrayOutputStream out = new ByteArrayOutputStream();
         final ByteArrayOutputStream err = new ByteArrayOutputStream();
         final PumpStreamHandler streamHandler = new PumpStreamHandler(out, err);
@@ -60,7 +66,9 @@ public class CommandUtils {
         executor.setStreamHandler(streamHandler);
         executor.setExitValues(new int[] {0});
         try {
-            executor.execute(commandLine);
+            Map<String, String> newEnv = new HashMap<>(System.getenv());
+            newEnv.putAll(env);
+            executor.execute(commandLine, newEnv);
             if (err.size() > 0) {
                 log.warn(StringUtils.trim(err.toString()));
             }
