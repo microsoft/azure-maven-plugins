f92ab3f0e108b92bc1ae4c015391c279fb37fa87
Mingliang Wang <wangmi@microsoft.com>
2021-07-19 00:22:15 +0800

    support AzureText in AzureMessager/AzureMessage.

--- azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/messager/IAzureMessager.java
+++ azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/messager/IAzureMessager.java
@@ -1,41 +1,72 @@
 package com.microsoft.azure.toolkit.lib.common.messager;
 
+import com.microsoft.azure.toolkit.lib.common.bundle.AzureText;
+
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.Optional;
 
 public interface IAzureMessager {
     String DEFAULT_TITLE = "Azure";
 
     default void success(@Nonnull String message, String title, IAzureMessage.Action... actions) {
-        this.show(new SimpleMessage(IAzureMessage.Type.SUCCESS, message).setActions(actions).setTitle(title));
+        this.show(this.buildMessage(IAzureMessage.Type.SUCCESS, AzureText.fromText(message), title, actions, null));
+    }
+
+    default void success(@Nonnull AzureText message, String title, IAzureMessage.Action... actions) {
+        this.show(this.buildMessage(IAzureMessage.Type.SUCCESS, message, title, actions, null));
     }
 
     default void info(@Nonnull String message, String title, IAzureMessage.Action... actions) {
-        this.show(new SimpleMessage(IAzureMessage.Type.INFO, message).setActions(actions).setTitle(title));
+        this.show(this.buildMessage(IAzureMessage.Type.INFO, AzureText.fromText(message), title, actions, null));
+    }
+
+    default void info(@Nonnull AzureText message, String title, IAzureMessage.Action... actions) {
+        this.show(this.buildMessage(IAzureMessage.Type.INFO, message, title, actions, null));
     }
 
     default void warning(@Nonnull String message, String title, IAzureMessage.Action... actions) {
-        this.show(new SimpleMessage(IAzureMessage.Type.WARNING, message).setActions(actions).setTitle(title));
+        this.show(this.buildMessage(IAzureMessage.Type.WARNING, AzureText.fromText(message), title, actions, null));
+    }
+
+    default void warning(@Nonnull AzureText message, String title, IAzureMessage.Action... actions) {
+        this.show(this.buildMessage(IAzureMessage.Type.WARNING, message, title, actions, null));
     }
 
     default void error(@Nonnull String message, String title, IAzureMessage.Action... actions) {
-        this.show(new SimpleMessage(IAzureMessage.Type.ERROR, message).setActions(actions).setTitle(title));
+        this.show(this.buildMessage(IAzureMessage.Type.ERROR, AzureText.fromText(message), title, actions, null));
+    }
+
+    default void error(@Nonnull AzureText message, String title, IAzureMessage.Action... actions) {
+        this.show(this.buildMessage(IAzureMessage.Type.ERROR, message, title, actions, null));
     }
 
     default void error(@Nonnull Throwable throwable, String title, IAzureMessage.Action... actions) {
         final String message = Optional.ofNullable(throwable.getMessage()).orElse(throwable.getClass().getSimpleName());
-        this.show(new SimpleMessage(IAzureMessage.Type.ERROR, message).setPayload(throwable).setActions(actions).setTitle(title));
+        this.show(this.buildMessage(IAzureMessage.Type.ERROR, AzureText.fromText(message), title, actions, throwable));
     }
 
     default boolean confirm(@Nonnull String message, String title) {
-        return this.show(new SimpleMessage(IAzureMessage.Type.CONFIRM, message).setTitle(title));
+        return this.show(this.buildMessage(IAzureMessage.Type.CONFIRM, AzureText.fromText(message), title, null, null));
+    }
+
+    default boolean confirm(@Nonnull AzureText message, String title) {
+        return this.show(this.buildMessage(IAzureMessage.Type.CONFIRM, message, title, null, null));
     }
 
     default void alert(@Nonnull String message, String title) {
-        this.show(new SimpleMessage(IAzureMessage.Type.ALERT, message).setTitle(title));
+        this.show(this.buildMessage(IAzureMessage.Type.ALERT, AzureText.fromText(message), title, null, null));
+    }
+
+    default void alert(@Nonnull AzureText message, String title) {
+        this.show(this.buildMessage(IAzureMessage.Type.ALERT, message, title, null, null));
     }
 
     default void success(@Nonnull String message, String title) {
+        this.success(message, title, new IAzureMessage.Action[0]);
+    }
+
+    default void success(@Nonnull AzureText message, String title) {
         this.success(message, title, new IAzureMessage.Action[0]);
     }
 
@@ -43,11 +74,23 @@ public interface IAzureMessager {
         this.info(message, title, new IAzureMessage.Action[0]);
     }
 
+    default void info(@Nonnull AzureText message, String title) {
+        this.info(message, title, new IAzureMessage.Action[0]);
+    }
+
     default void warning(@Nonnull String message, String title) {
         this.warning(message, title, new IAzureMessage.Action[0]);
     }
 
+    default void warning(@Nonnull AzureText message, String title) {
+        this.warning(message, title, new IAzureMessage.Action[0]);
+    }
+
     default void error(@Nonnull String message, String title) {
+        this.error(message, title, new IAzureMessage.Action[0]);
+    }
+
+    default void error(@Nonnull AzureText message, String title) {
         this.error(message, title, new IAzureMessage.Action[0]);
     }
 
@@ -59,7 +102,15 @@ public interface IAzureMessager {
         return this.confirm(message, DEFAULT_TITLE);
     }
 
+    default boolean confirm(@Nonnull AzureText message) {
+        return this.confirm(message, DEFAULT_TITLE);
+    }
+
     default void alert(@Nonnull String message) {
+        this.alert(message, DEFAULT_TITLE);
+    }
+
+    default void alert(@Nonnull AzureText message) {
         this.alert(message, DEFAULT_TITLE);
     }
 
@@ -67,7 +118,15 @@ public interface IAzureMessager {
         this.success(message, DEFAULT_TITLE);
     }
 
+    default void success(@Nonnull AzureText message) {
+        this.success(message, DEFAULT_TITLE);
+    }
+
     default void info(@Nonnull String message) {
+        this.info(message, DEFAULT_TITLE);
+    }
+
+    default void info(@Nonnull AzureText message) {
         this.info(message, DEFAULT_TITLE);
     }
 
@@ -75,7 +134,15 @@ public interface IAzureMessager {
         this.warning(message, DEFAULT_TITLE);
     }
 
+    default void warning(@Nonnull AzureText message) {
+        this.warning(message, DEFAULT_TITLE);
+    }
+
     default void error(@Nonnull String message) {
+        this.error(message, DEFAULT_TITLE);
+    }
+
+    default void error(@Nonnull AzureText message) {
         this.error(message, DEFAULT_TITLE);
     }
 
@@ -83,8 +150,13 @@ public interface IAzureMessager {
         this.error(throwable, DEFAULT_TITLE);
     }
 
-    default String value(String val) {
-        return val;
+    default IAzureMessage buildMessage(@Nonnull IAzureMessage.Type type, @Nonnull AzureText content,
+                                       @Nullable String title, @Nullable IAzureMessage.Action[] actions, @Nullable Object payload) {
+        final AzureMessage message = new AzureMessage(type, content).setPayload(payload).setActions(actions).setTitle(title);
+        if (this instanceof IAzureMessage.ValueDecorator) {
+            message.setValueDecorator((IAzureMessage.ValueDecorator) this);
+        }
+        return message;
     }
 
     boolean show(IAzureMessage message);
