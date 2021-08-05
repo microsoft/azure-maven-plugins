6bc8db8337062da82182359a211fd9f68351b9a0
Mingliang Wang <wangmi@microsoft.com>
2021-08-06 00:11:41 +0800

    improve DataStore to support any type of fields

--- azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/DataStore.java
+++ azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/DataStore.java
@@ -14,33 +14,58 @@ import java.util.WeakHashMap;
 /**
  * better not override equals() and hashcode() if use default get/set
  */
-public interface DataStore<T> {
+public interface DataStore {
     @Nonnull
     @SuppressWarnings("unchecked")
-    default <D extends T> D get(Class<D> type, @Nonnull D dft) {
+    default <D> D get(Class<D> type, @Nonnull D dft) {
         synchronized (Impl.store) {
-            final Map<Class<?>, Object> thisStore = Impl.store.computeIfAbsent(this, (k) -> new HashMap<>());
+            final Map<Object, Object> thisStore = Impl.store.computeIfAbsent(this, (k) -> new HashMap<>());
             return (D) thisStore.computeIfAbsent(type, (t) -> dft);
         }
     }
 
     @Nullable
     @SuppressWarnings("unchecked")
-    default <D extends T> D get(Class<D> type) {
+    default <D> D get(Class<D> type) {
         synchronized (Impl.store) {
-            final Map<Class<?>, Object> thisStore = Impl.store.computeIfAbsent(this, (k) -> new HashMap<>());
+            final Map<Object, Object> thisStore = Impl.store.computeIfAbsent(this, (k) -> new HashMap<>());
             return (D) thisStore.get(type);
         }
     }
 
-    default <D extends T> void set(Class<D> type, D val) {
+    default <D> void set(Class<D> type, D val) {
         synchronized (Impl.store) {
-            final Map<Class<?>, Object> thisStore = Impl.store.computeIfAbsent(this, (k) -> new HashMap<>());
+            final Map<Object, Object> thisStore = Impl.store.computeIfAbsent(this, (k) -> new HashMap<>());
             thisStore.put(type, val);
+        }
+    }
+
+    @Nonnull
+    @SuppressWarnings("unchecked")
+    default <D> D get(String key, @Nonnull D dft) {
+        synchronized (Impl.store) {
+            final Map<Object, Object> thisStore = Impl.store.computeIfAbsent(this, (k) -> new HashMap<>());
+            return (D) thisStore.computeIfAbsent(key, (t) -> dft);
+        }
+    }
+
+    @Nullable
+    @SuppressWarnings("unchecked")
+    default <D> D get(String key) {
+        synchronized (Impl.store) {
+            final Map<Object, Object> thisStore = Impl.store.computeIfAbsent(this, (k) -> new HashMap<>());
+            return (D) thisStore.get(key);
+        }
+    }
+
+    default <D> void set(String key, D val) {
+        synchronized (Impl.store) {
+            final Map<Object, Object> thisStore = Impl.store.computeIfAbsent(this, (k) -> new HashMap<>());
+            thisStore.put(key, val);
         }
     }
 }
 
 final class Impl {
-    static final WeakHashMap<Object, Map<Class<?>, Object>> store = new WeakHashMap<>();
+    static final WeakHashMap<Object, Map<Object, Object>> store = new WeakHashMap<>();
 }
