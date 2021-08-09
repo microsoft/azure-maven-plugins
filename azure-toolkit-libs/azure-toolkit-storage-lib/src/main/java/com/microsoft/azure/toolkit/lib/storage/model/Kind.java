292cabae2b7230f8ad6630c54f7812bd2ba12bf1
shenqianjin <qianjinshen@microsoft.com>
2021-08-09 14:47:41 +0800

    support general purpose v1 storage account for vm.

--- azure-toolkit-libs/azure-toolkit-storage-lib/src/main/java/com/microsoft/azure/toolkit/lib/storage/model/Kind.java
+++ azure-toolkit-libs/azure-toolkit-storage-lib/src/main/java/com/microsoft/azure/toolkit/lib/storage/model/Kind.java
@@ -18,6 +18,7 @@ import java.util.List;
 @EqualsAndHashCode
 public class Kind implements ExpandableParameter {
 
+    public static final Kind STORAGE = new Kind(Performance.STANDARD, "Storage", "General Purpose v1");
     public static final Kind STORAGE_V2 = new Kind(Performance.STANDARD, "StorageV2", "General Purpose v2");
     public static final Kind BLOB_STORAGE = new Kind(Performance.PREMIUM, "BlobStorage", "Blob Storage");
     public static final Kind BLOCK_BLOB_STORAGE = new Kind(Performance.PREMIUM, "BlockBlobStorage", "Block Blobs Storage");
