e04f79d2e9bd7e0db667985059a7f0bd5e1af415
Mingliang Wang <wangmi@microsoft.com>
2021-08-10 12:07:50 +0800

    add source as a required field to AzureEvent

--- azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/event/AzureOperationEvent.java
+++ azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/event/AzureOperationEvent.java
@@ -8,24 +8,23 @@ package com.microsoft.azure.toolkit.lib.common.event;
 import com.microsoft.azure.toolkit.lib.common.operation.AzureOperationRef;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import javax.annotation.Nonnull;
 
 @Getter
+@Setter
 @RequiredArgsConstructor
-public class AzureOperationEvent<T extends AzureOperationEvent.Source<T>> implements AzureEvent<T> {
+public class AzureOperationEvent<T extends AzureOperationEvent.Source<T>> implements AzureEvent<Object> {
     private final T source;
     private final AzureOperationRef operation;
     private final Stage stage;
+    private Object payload;
 
     @Nonnull
     @Override
     public String getType() {
         return operation.getName();
-    }
-
-    public T getPayload() {
-        return this.source;
     }
 
     public interface Source<T> {
