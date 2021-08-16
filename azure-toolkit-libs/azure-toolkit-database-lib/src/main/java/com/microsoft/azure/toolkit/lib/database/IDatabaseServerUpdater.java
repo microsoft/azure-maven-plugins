168f0e8d0314fe7728980e81f8234d892fd232cb
shenqianjin <qianjinshen@microsoft.com>
2021-08-16 17:01:19 +0800

    fix checkstyle issue.

--- azure-toolkit-libs/azure-toolkit-database-lib/src/main/java/com/microsoft/azure/toolkit/lib/database/IDatabaseServerUpdater.java
+++ azure-toolkit-libs/azure-toolkit-database-lib/src/main/java/com/microsoft/azure/toolkit/lib/database/IDatabaseServerUpdater.java
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
+
 package com.microsoft.azure.toolkit.lib.database;
 
 import com.microsoft.azure.toolkit.lib.common.task.ICommittable;
--- azure-toolkit-libs/azure-toolkit-database-lib/src/main/java/com/microsoft/azure/toolkit/lib/database/entity/IDatabaseServerEntity.java
+++ azure-toolkit-libs/azure-toolkit-database-lib/src/main/java/com/microsoft/azure/toolkit/lib/database/entity/IDatabaseServerEntity.java
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
+
 package com.microsoft.azure.toolkit.lib.database.entity;
 
 import com.microsoft.azure.toolkit.lib.common.entity.IAzureResourceEntity;
@@ -10,12 +11,22 @@ import com.microsoft.azure.toolkit.lib.common.model.Region;
 public interface IDatabaseServerEntity extends IAzureResourceEntity {
 
     String getResourceGroupName();
+
     Region getRegion();
+
     String getAdministratorLoginName();
+
     String getFullyQualifiedDomainName();
+
     boolean isEnableAccessFromAzureServices();
+
     boolean isEnableAccessFromLocalMachine();
+
     default String getState() {
+        return null;
+    }
+
+    default String getVersion() {
         return null;
     }
 
