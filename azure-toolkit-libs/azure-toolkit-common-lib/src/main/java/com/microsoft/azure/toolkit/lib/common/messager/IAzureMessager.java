28383985cc122f94d762b7ef17b999836794407e
Mingliang Wang <wangmi@microsoft.com>
2021-09-06 17:08:04 +0800

    remove IAzureMessage.Action and use new action framework

--- azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/messager/IAzureMessager.java
+++ azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/messager/IAzureMessager.java
@@ -1,5 +1,11 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
 package com.microsoft.azure.toolkit.lib.common.messager;
 
+import com.microsoft.azure.toolkit.lib.common.action.Action;
 import com.microsoft.azure.toolkit.lib.common.bundle.AzureString;
 
 import javax.annotation.Nonnull;
@@ -9,39 +15,39 @@ import java.util.Optional;
 public interface IAzureMessager {
     String DEFAULT_TITLE = "Azure";
 
-    default void success(@Nonnull String message, String title, IAzureMessage.Action... actions) {
+    default void success(@Nonnull String message, String title, Action<?>... actions) {
         this.show(this.buildMessage(IAzureMessage.Type.SUCCESS, AzureString.fromString(message), title, actions, null));
     }
 
-    default void success(@Nonnull AzureString message, String title, IAzureMessage.Action... actions) {
+    default void success(@Nonnull AzureString message, String title, Action<?>... actions) {
         this.show(this.buildMessage(IAzureMessage.Type.SUCCESS, message, title, actions, null));
     }
 
-    default void info(@Nonnull String message, String title, IAzureMessage.Action... actions) {
+    default void info(@Nonnull String message, String title, Action<?>... actions) {
         this.show(this.buildMessage(IAzureMessage.Type.INFO, AzureString.fromString(message), title, actions, null));
     }
 
-    default void info(@Nonnull AzureString message, String title, IAzureMessage.Action... actions) {
+    default void info(@Nonnull AzureString message, String title, Action<?>... actions) {
         this.show(this.buildMessage(IAzureMessage.Type.INFO, message, title, actions, null));
     }
 
-    default void warning(@Nonnull String message, String title, IAzureMessage.Action... actions) {
+    default void warning(@Nonnull String message, String title, Action<?>... actions) {
         this.show(this.buildMessage(IAzureMessage.Type.WARNING, AzureString.fromString(message), title, actions, null));
     }
 
-    default void warning(@Nonnull AzureString message, String title, IAzureMessage.Action... actions) {
+    default void warning(@Nonnull AzureString message, String title, Action<?>... actions) {
         this.show(this.buildMessage(IAzureMessage.Type.WARNING, message, title, actions, null));
     }
 
-    default void error(@Nonnull String message, String title, IAzureMessage.Action... actions) {
+    default void error(@Nonnull String message, String title, Action<?>... actions) {
         this.show(this.buildMessage(IAzureMessage.Type.ERROR, AzureString.fromString(message), title, actions, null));
     }
 
-    default void error(@Nonnull AzureString message, String title, IAzureMessage.Action... actions) {
+    default void error(@Nonnull AzureString message, String title, Action<?>... actions) {
         this.show(this.buildMessage(IAzureMessage.Type.ERROR, message, title, actions, null));
     }
 
-    default void error(@Nonnull Throwable throwable, String title, IAzureMessage.Action... actions) {
+    default void error(@Nonnull Throwable throwable, String title, Action<?>... actions) {
         final String message = Optional.ofNullable(throwable.getMessage()).orElse(throwable.getClass().getSimpleName());
         this.show(this.buildMessage(IAzureMessage.Type.ERROR, AzureString.fromString(message), title, actions, throwable));
     }
@@ -63,39 +69,39 @@ public interface IAzureMessager {
     }
 
     default void success(@Nonnull String message, String title) {
-        this.success(message, title, new IAzureMessage.Action[0]);
+        this.success(message, title, new Action<?>[0]);
     }
 
     default void success(@Nonnull AzureString message, String title) {
-        this.success(message, title, new IAzureMessage.Action[0]);
+        this.success(message, title, new Action<?>[0]);
     }
 
     default void info(@Nonnull String message, String title) {
-        this.info(message, title, new IAzureMessage.Action[0]);
+        this.info(message, title, new Action<?>[0]);
     }
 
     default void info(@Nonnull AzureString message, String title) {
-        this.info(message, title, new IAzureMessage.Action[0]);
+        this.info(message, title, new Action<?>[0]);
     }
 
     default void warning(@Nonnull String message, String title) {
-        this.warning(message, title, new IAzureMessage.Action[0]);
+        this.warning(message, title, new Action<?>[0]);
     }
 
     default void warning(@Nonnull AzureString message, String title) {
-        this.warning(message, title, new IAzureMessage.Action[0]);
+        this.warning(message, title, new Action<?>[0]);
     }
 
     default void error(@Nonnull String message, String title) {
-        this.error(message, title, new IAzureMessage.Action[0]);
+        this.error(message, title, new Action<?>[0]);
     }
 
     default void error(@Nonnull AzureString message, String title) {
-        this.error(message, title, new IAzureMessage.Action[0]);
+        this.error(message, title, new Action<?>[0]);
     }
 
     default void error(@Nonnull Throwable throwable, String title) {
-        this.error(throwable, title, new IAzureMessage.Action[0]);
+        this.error(throwable, title, new Action<?>[0]);
     }
 
     default boolean confirm(@Nonnull String message) {
@@ -151,7 +157,7 @@ public interface IAzureMessager {
     }
 
     default IAzureMessage buildMessage(@Nonnull IAzureMessage.Type type, @Nonnull AzureString content,
-                                       @Nullable String title, @Nullable IAzureMessage.Action[] actions, @Nullable Object payload) {
+                                       @Nullable String title, @Nullable Action<?>[] actions, @Nullable Object payload) {
         final AzureMessage message = new AzureMessage(type, content).setPayload(payload).setActions(actions).setTitle(title);
         if (this instanceof IAzureMessage.ValueDecorator) {
             message.setValueDecorator((IAzureMessage.ValueDecorator) this);
