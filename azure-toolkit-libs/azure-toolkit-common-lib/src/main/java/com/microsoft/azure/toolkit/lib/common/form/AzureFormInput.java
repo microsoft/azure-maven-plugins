a347de918592e127ff7d1b037f98753acc60b57d
Mingliang Wang <wangmi@microsoft.com>
2021-10-18 16:09:22 +0800

    reduce duplicate code for AzureFormInput component

--- azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/form/AzureFormInput.java
+++ azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/form/AzureFormInput.java
@@ -5,18 +5,44 @@
 
 package com.microsoft.azure.toolkit.lib.common.form;
 
+import com.microsoft.azure.toolkit.lib.common.DataStore;
+import com.microsoft.azure.toolkit.lib.common.task.AzureTaskManager;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Consumer;
 
-public interface AzureFormInput<T> extends Validatable {
+public interface AzureFormInput<T> extends Validatable, DataStore {
 
     String MSG_REQUIRED = "This field is required.";
 
     T getValue();
 
     void setValue(final T val);
+
+    default void addValueChangedListener(Consumer<T> listener) {
+        final CopyOnWriteArrayList<Consumer<T>> valueChangedListeners = this.get("valueChangedListeners", new CopyOnWriteArrayList<>());
+        if (!valueChangedListeners.contains(listener)) {
+            valueChangedListeners.add(listener);
+        }
+    }
+
+    default void removeValueChangedListener(Consumer<T> listener) {
+        final CopyOnWriteArrayList<Consumer<T>> valueChangedListeners = this.get("valueChangedListeners", new CopyOnWriteArrayList<>());
+        valueChangedListeners.remove(listener);
+    }
+
+    default List<Consumer<T>> getValueChangedListeners() {
+        return this.get("valueChangedListeners", new CopyOnWriteArrayList<>());
+    }
+
+    default void fireValueChangedEvent(T val) {
+        AzureTaskManager.getInstance().runLater(() -> this.getValueChangedListeners().forEach(l -> l.accept(val)));
+    }
 
     @Nonnull
     default AzureValidationInfo doValidate() {
@@ -32,11 +58,25 @@ public interface AzureFormInput<T> extends Validatable {
         return Validatable.super.doValidate();
     }
 
-    default boolean isRequired() {
-        return false;
+    default String getLabel() {
+        return this.getClass().getSimpleName();
     }
 
-    default String getLabel() {
-        return "";
+    @Nullable
+    @Override
+    default Validatable.Validator getValidator() {
+        return this.get("validator");
+    }
+
+    default void setValidator(Validator validator) {
+        this.set("validator", validator);
+    }
+
+    default boolean isRequired() {
+        return this.get("required", false);
+    }
+
+    default void setRequired(boolean required) {
+        this.set("required", required);
     }
 }
