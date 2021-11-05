eb9bedd7c833439707f58bcf99875f50a1a413bf
Mingliang Wang <wangmi@microsoft.com>
2021-11-05 23:27:48 +0800

    #1891865: make `AzureForm`(composition of `AzureFormInput`) implement `AzureFormInput`

--- azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/form/AzureForm.java
+++ azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/form/AzureForm.java
@@ -5,17 +5,68 @@
 
 package com.microsoft.azure.toolkit.lib.common.form;
 
+import reactor.core.publisher.Flux;
+
+import javax.annotation.Nonnull;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
-public interface AzureForm<T> {
-    T getData();
+public interface AzureForm<T> extends AzureFormInput<T> {
+    default T getValue() {
+        return this.getData();
+    }
 
-    void setData(T data);
+    default void setValue(T data) {
+        setData(data);
+    }
+
+    @Override
+    @Nonnull
+    default AzureValidationInfo validateInternal(T value) {
+        final List<AzureValidationInfo> infos = this.validateAllInputs();
+        assert infos.size() == this.getInputs().size() : "some inputs have no validation info.";
+        final AzureValidationInfo info = infos.stream()
+            .min(Comparator.comparing(AzureValidationInfo::getType))
+            .orElse(AzureValidationInfo.pending(this));
+        this.setValidationInfo(info);
+        return info;
+    }
+
+    default List<AzureValidationInfo> validateAllInputs() {
+        return this.validateAllInputsAsync().collectList().block();
+    }
+
+    default Flux<AzureValidationInfo> validateAllInputsAsync() {
+        return Flux.fromIterable(this.getInputs()).flatMap(i -> i.validateValueAsync().flux());
+    }
+
+    default List<AzureValidationInfo> getAllValidationInfos(final boolean revalidateIfNone) {
+        return this.getInputs().stream()
+            .map(input -> input.getValidationInfo(revalidateIfNone))
+            .filter(Objects::nonNull).collect(Collectors.toList());
+    }
 
     List<AzureFormInput<?>> getInputs();
 
+    /**
+     * implement/use {@link #getValue()} instead
+     */
+    @Deprecated
+    T getData();
+
+    /**
+     * implement/use {@link #setValue(Object)} instead
+     */
+    @Deprecated
+    void setData(T data);
+
+    /**
+     * use {@link #validateAllInputs()} instead
+     */
+    @Deprecated
     default List<AzureValidationInfo> validateData() {
-        return this.getInputs().stream().map(AzureFormInput::doValidate).collect(Collectors.toList());
+        return this.validateAllInputs();
     }
 }
