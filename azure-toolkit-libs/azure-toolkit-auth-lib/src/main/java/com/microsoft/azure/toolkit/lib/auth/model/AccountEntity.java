c4e1f605e8e13278460bac029e698af7e263582d
Mingliang Wang <wangmi@microsoft.com>
2022-05-09 16:03:23 +0800

    sort subscriptions

--- azure-toolkit-libs/azure-toolkit-auth-lib/src/main/java/com/microsoft/azure/toolkit/lib/auth/model/AccountEntity.java
+++ azure-toolkit-libs/azure-toolkit-auth-lib/src/main/java/com/microsoft/azure/toolkit/lib/auth/model/AccountEntity.java
@@ -7,10 +7,13 @@ package com.microsoft.azure.toolkit.lib.auth.model;
 
 import com.azure.core.management.AzureEnvironment;
 import com.microsoft.azure.toolkit.lib.common.model.Subscription;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -24,6 +27,7 @@ public class AccountEntity {
 
     private String email;
 
+    @Setter(AccessLevel.NONE)
     private List<Subscription> subscriptions;
 
     private List<String> selectedSubscriptionIds;
@@ -33,4 +37,10 @@ public class AccountEntity {
     private boolean available;
 
     private Throwable lastError;
+
+    public void setSubscriptions(List<Subscription> subscriptions) {
+        this.subscriptions = subscriptions.stream()
+            .sorted(Comparator.comparing(s -> s.getName().toLowerCase()))
+            .collect(Collectors.toList());
+    }
 }
