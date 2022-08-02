ce2bcac6f4be233ac7fd55d880f524b354490202
Mingliang Wang <wangmi@microsoft.com>
2022-08-02 10:22:31 +0800

    merge appservice.JsonUtils into common.JsonUtils.

--- azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/utils/JsonUtils.java
+++ azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/utils/JsonUtils.java
@@ -9,8 +9,35 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Writer;
+
 public class JsonUtils {
     private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    public static <T> T deepCopyWithJson(T source) {
+        //noinspection unchecked
+        return (T) fromJson(toJson(source), source.getClass());
+    }
+
+    public static void writeToJsonFile(File targetFile, Object json) throws IOException {
+        try (Writer writer = new FileWriter(targetFile)) {
+            MAPPER.writeValue(writer, json);
+        }
+    }
+
+    public static <T> T readFromJsonFile(File target, Class<T> clazz) {
+        try (FileInputStream fis = new FileInputStream(target);
+             InputStreamReader isr = new InputStreamReader(fis)) {
+            return MAPPER.readValue(isr, clazz);
+        } catch (IOException e) {
+            return null;
+        }
+    }
 
     @SneakyThrows
     public static <T> T fromJson(String json, Class<T> clazz) {
