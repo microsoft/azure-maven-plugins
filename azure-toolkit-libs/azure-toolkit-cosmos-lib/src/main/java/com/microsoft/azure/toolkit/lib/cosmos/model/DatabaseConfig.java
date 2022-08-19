d9dc76dfc0f7182e5b42d6f5fdf132fe46d56a1e
Hanxiao Liu <hanli@microsoft.com>
2022-08-19 17:26:37 +0800

    Add default config for cosmos account/database

--- azure-toolkit-libs/azure-toolkit-cosmos-lib/src/main/java/com/microsoft/azure/toolkit/lib/cosmos/model/DatabaseConfig.java
+++ azure-toolkit-libs/azure-toolkit-cosmos-lib/src/main/java/com/microsoft/azure/toolkit/lib/cosmos/model/DatabaseConfig.java
@@ -5,6 +5,7 @@
 
 package com.microsoft.azure.toolkit.lib.cosmos.model;
 
+import com.microsoft.azure.toolkit.lib.common.utils.Utils;
 import lombok.Data;
 
 @Data
@@ -12,4 +13,11 @@ public class DatabaseConfig {
     private String name;
     private Integer throughput;
     private Integer maxThroughput;
+
+    public static DatabaseConfig getDefaultDatabaseConfig() {
+        final DatabaseConfig result = new DatabaseConfig();
+        result.setName(String.format("database%s", Utils.getTimestamp()));
+        result.setMaxThroughput(4000);
+        return result;
+    }
 }
