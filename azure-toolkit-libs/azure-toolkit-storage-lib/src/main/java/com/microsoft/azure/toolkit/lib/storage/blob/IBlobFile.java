76b627b7a4b0f58445e648b5eeadc26df8e80b71
Mingliang Wang <wangmi@microsoft.com>
2022-10-20 15:11:36 +0800

    AB#1998162: generate SAS url.

--- azure-toolkit-libs/azure-toolkit-storage-lib/src/main/java/com/microsoft/azure/toolkit/lib/storage/blob/IBlobFile.java
+++ azure-toolkit-libs/azure-toolkit-storage-lib/src/main/java/com/microsoft/azure/toolkit/lib/storage/blob/IBlobFile.java
@@ -6,12 +6,33 @@
 package com.microsoft.azure.toolkit.lib.storage.blob;
 
 import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.sas.BlobContainerSasPermission;
+import com.azure.storage.blob.sas.BlobSasPermission;
+import com.azure.storage.blob.sas.BlobServiceSasSignatureValues;
+import com.azure.storage.common.sas.SasProtocol;
 import com.microsoft.azure.toolkit.lib.storage.model.StorageFile;
 
+import java.time.OffsetDateTime;
+
 public interface IBlobFile extends StorageFile {
+
     BlobContainer getContainer();
 
     BlobContainerClient getClient();
 
-    String getPath();
+    default String getSasUrl() {
+        final OffsetDateTime expiration = OffsetDateTime.now().plusDays(1);
+        final String containerUrl = this.getContainer().getUrl();
+        final String token;
+        if (this instanceof BlobContainer) {
+            final BlobContainerSasPermission containerPermission = new BlobContainerSasPermission().setReadPermission(true).setListPermission(true);
+            final BlobServiceSasSignatureValues builder = new BlobServiceSasSignatureValues(expiration, containerPermission).setProtocol(SasProtocol.HTTPS_ONLY);
+            token = this.getClient().generateSas(builder);
+        } else {
+            final BlobSasPermission blobPermission = new BlobSasPermission().setReadPermission(true).setListPermission(true);
+            final BlobServiceSasSignatureValues builder = new BlobServiceSasSignatureValues(expiration, blobPermission).setProtocol(SasProtocol.HTTPS_ONLY);
+            token = this.getClient().generateSas(builder);
+        }
+        return String.format("%s/%s?%s", containerUrl, this.getPath(), token);
+    }
 }
