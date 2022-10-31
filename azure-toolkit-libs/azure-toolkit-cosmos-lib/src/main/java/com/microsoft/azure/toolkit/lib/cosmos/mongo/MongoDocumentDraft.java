dcadc71b122129a933bafe1397feb183221821d2
Hanxiao Liu <hanli@microsoft.com>
2022-10-31 21:56:16 +0800

    Fix logic to get shared key in mongo document draft

--- azure-toolkit-libs/azure-toolkit-cosmos-lib/src/main/java/com/microsoft/azure/toolkit/lib/cosmos/mongo/MongoDocumentDraft.java
+++ azure-toolkit-libs/azure-toolkit-cosmos-lib/src/main/java/com/microsoft/azure/toolkit/lib/cosmos/mongo/MongoDocumentDraft.java
@@ -15,6 +15,7 @@ import lombok.Setter;
 import org.bson.Document;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -48,6 +49,16 @@ public class MongoDocumentDraft extends MongoDocument implements
         return Optional.ofNullable(draftDocument)
                 .map(draftDocument -> draftDocument.get(MONGO_ID_KEY))
                 .orElseGet(super::getDocumentId);
+    }
+
+    @Nullable
+    @Override
+    public String getSharedKey() {
+        final String sharedKey = getParent().getSharedKey();
+        return Optional.ofNullable(draftDocument)
+                .map(node -> node.get(sharedKey))
+                .map(Object::toString)
+                .orElseGet(super::getSharedKey);
     }
 
     @Override
