45c9e1d13e78644fd73d4e504b21cad3e8414770
yuwe <yuwe@microsoft.com>
2022-11-16 16:18:17 +0800

    AB#2009408 use name and resourceId for equals()

--- azure-toolkit-libs/azure-toolkit-applicationinsights-lib/src/main/java/com/microsoft/azure/toolkit/lib/applicationinsights/workspace/LogAnalyticsWorkspaceConfig.java
+++ azure-toolkit-libs/azure-toolkit-applicationinsights-lib/src/main/java/com/microsoft/azure/toolkit/lib/applicationinsights/workspace/LogAnalyticsWorkspaceConfig.java
@@ -1,14 +1,20 @@
 package com.microsoft.azure.toolkit.lib.applicationinsights.workspace;
 
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @SuperBuilder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class LogAnalyticsWorkspaceConfig {
     private boolean newCreate;
+    @EqualsAndHashCode.Include
     private String name;
+    @EqualsAndHashCode.Include
     private String resourceId;
     private String subscriptionId;
+    private String regionName;
 }
