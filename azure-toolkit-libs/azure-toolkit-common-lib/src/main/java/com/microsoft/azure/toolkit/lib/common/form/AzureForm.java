89bc42372235872052d5fc5455415894b56bd01f
Mingliang Wang <wangmi@microsoft.com>
2022-12-19 14:56:05 +0800

    add support for validating additional info, e.g. combination of inputs.

--- azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/form/AzureForm.java
+++ azure-toolkit-libs/azure-toolkit-common-lib/src/main/java/com/microsoft/azure/toolkit/lib/common/form/AzureForm.java
@@ -14,6 +14,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.microsoft.azure.toolkit.lib.common.form.AzureValidationInfo.Type.SUCCESS;
 
@@ -35,17 +36,17 @@ public interface AzureForm<T> extends AzureFormInput<T> {
     }
 
     default Flux<AzureValidationInfo> validateAllInputsAsync() {
-        return Flux.fromIterable(this.getInputs()).flatMap(i -> i.validateValueAsync().flux());
+        return Flux.fromIterable(this.getInputs()).flatMap(i -> i.validateValueAsync().flux()).concatWith(Flux.fromIterable(this.validateAdditionalInfo()));
     }
 
     default List<AzureValidationInfo> getAllValidationInfos(final boolean revalidateIfNone) {
-        return this.getInputs().stream().map(input -> input.getValidationInfo(revalidateIfNone))
+        return Stream.concat(this.getInputs().stream().map(input -> input.getValidationInfo(revalidateIfNone)), this.validateAdditionalInfo().stream())
             .filter(Objects::nonNull).collect(Collectors.toList());
     }
 
     @Override
     default AzureValidationInfo getValidationInfo() {
-        return this.getInputs().stream().map(input -> input.getValidationInfo(true))
+        return Stream.concat(this.getInputs().stream().map(input -> input.getValidationInfo(true)), this.validateAdditionalInfo().stream())
             .filter(i -> i.getType() != SUCCESS)
             .min(Comparator.comparing(AzureValidationInfo::getType))
             .orElse(AzureValidationInfo.ok(this));
@@ -58,10 +59,10 @@ public interface AzureForm<T> extends AzureFormInput<T> {
     List<AzureFormInput<?>> getInputs();
 
     /**
-     * use {@link #validateAllInputs()} instead
+     * validate additional info.
+     * e.g. validate combination of inputs, such as password and confirm password
      */
-    @Deprecated
-    default List<AzureValidationInfo> validateData() {
-        return this.validateAllInputs();
+    default List<AzureValidationInfo> validateAdditionalInfo() {
+        return Collections.emptyList();
     }
 }
