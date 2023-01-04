f828b7e033656d497e45987eb4cc54acd064bee8
Hanxiao Liu <hanli@microsoft.com>
2023-01-04 21:01:45 +0800

    Support check name availability for container app in app environment

--- azure-toolkit-libs/azure-toolkit-containerapps-lib/src/main/java/com/microsoft/azure/toolkit/lib/containerapps/environment/ContainerAppsEnvironment.java
+++ azure-toolkit-libs/azure-toolkit-containerapps-lib/src/main/java/com/microsoft/azure/toolkit/lib/containerapps/environment/ContainerAppsEnvironment.java
@@ -5,12 +5,17 @@
 
 package com.microsoft.azure.toolkit.lib.containerapps.environment;
 
+import com.azure.resourcemanager.appcontainers.models.CheckNameAvailabilityReason;
+import com.azure.resourcemanager.appcontainers.models.CheckNameAvailabilityRequest;
+import com.azure.resourcemanager.appcontainers.models.CheckNameAvailabilityResponse;
 import com.azure.resourcemanager.appcontainers.models.ManagedEnvironment;
 import com.azure.resourcemanager.resources.fluentcore.arm.ResourceId;
 import com.microsoft.azure.toolkit.lib.Azure;
 import com.microsoft.azure.toolkit.lib.common.model.AbstractAzResource;
 import com.microsoft.azure.toolkit.lib.common.model.AbstractAzResourceModule;
+import com.microsoft.azure.toolkit.lib.common.model.Availability;
 import com.microsoft.azure.toolkit.lib.common.model.Deletable;
+import com.microsoft.azure.toolkit.lib.common.operation.AzureOperation;
 import com.microsoft.azure.toolkit.lib.containerapps.AzureContainerApps;
 import com.microsoft.azure.toolkit.lib.containerapps.AzureContainerAppsServiceSubscription;
 import com.microsoft.azure.toolkit.lib.containerapps.containerapp.ContainerApp;
@@ -18,6 +23,8 @@ import com.microsoft.azure.toolkit.lib.containerapps.containerapp.ContainerApp;
 import javax.annotation.Nonnull;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 public class ContainerAppsEnvironment extends AbstractAzResource<ContainerAppsEnvironment, AzureContainerAppsServiceSubscription, ManagedEnvironment> implements Deletable {
     protected ContainerAppsEnvironment(@Nonnull String name, @Nonnull String resourceGroupName, @Nonnull ContainerAppsEnvironmentModule module) {
@@ -40,6 +47,14 @@ public class ContainerAppsEnvironment extends AbstractAzResource<ContainerAppsEn
 
     public List<ContainerApp> listContainerApps() {
         return Azure.az(AzureContainerApps.class).containerApps(this.getSubscriptionId()).listContainerAppsByEnvironment(this);
+    }
+
+    @AzureOperation(name = "azure/containerapps.check_name.name", params = "name")
+    public Availability checkContainerAppNameAvailability(String name) {
+        final CheckNameAvailabilityRequest request = new CheckNameAvailabilityRequest().withName(name).withType("Microsoft.App/containerApps");
+        final CheckNameAvailabilityResponse result = Objects.requireNonNull(this.getModule().getParent().getRemote())
+                .namespaces().checkNameAvailability(this.getResourceGroupName(), this.getName(), request);
+        return new Availability(result.nameAvailable(), Optional.ofNullable(result.reason()).map(CheckNameAvailabilityReason::toString).orElse(null), result.message());
     }
 
     @Nonnull
