a20df32f9c3c7c2d5ff7540cdea094636c87f7c4
yuwe <yuwe@microsoft.com>
2023-02-14 15:41:48 +0800

    add event hubs

---
+++ azure-toolkit-libs/azure-toolkit-eventhubs-lib/src/main/java/com/microsoft/azure/toolkit/lib/eventhubs/EventHubsNamespace.java
@@ -0,0 +1,39 @@
+package com.microsoft.azure.toolkit.lib.eventhubs;
+
+import com.azure.resourcemanager.eventhubs.models.EventHubNamespace;
+import com.azure.resourcemanager.resources.fluentcore.arm.ResourceId;
+import com.microsoft.azure.toolkit.lib.common.model.AbstractAzResource;
+import com.microsoft.azure.toolkit.lib.common.model.AbstractAzResourceModule;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.List;
+
+public class EventHubsNamespace extends AbstractAzResource<EventHubsNamespace, EventHubsNamespaceSubscription, EventHubNamespace> {
+    protected EventHubsNamespace(@Nonnull String name, @Nonnull String resourceGroupName, @Nonnull EventHubsNamespaceModule module) {
+        super(name, resourceGroupName, module);
+    }
+    
+    protected EventHubsNamespace(@Nonnull EventHubsNamespace namespace) {
+        super(namespace);
+    }
+
+    protected EventHubsNamespace(@Nonnull EventHubNamespace remote, @Nonnull EventHubsNamespaceModule module) {
+        super(remote.name(), ResourceId.fromString(remote.id()).resourceGroupName(), module);
+    }
+
+    @NotNull
+    @Override
+    public List<AbstractAzResourceModule<?, ?, ?>> getSubModules() {
+        return Collections.emptyList();
+    }
+
+    @NotNull
+    @Override
+    public String loadStatus(@NotNull EventHubNamespace remote) {
+        final String status = remote.innerModel().status();
+        getRemote().listEventHubs().forEach(eventHub -> eventHub.innerModel().status());
+        return remote.provisioningState();
+    }
+}
---
+++ azure-toolkit-libs/azure-toolkit-eventhubs-lib/src/main/java/com/microsoft/azure/toolkit/lib/eventhubs/EventHubsNamespaceSubscription.java
@@ -0,0 +1,47 @@
+package com.microsoft.azure.toolkit.lib.eventhubs;
+
+import com.azure.resourcemanager.eventhubs.EventHubsManager;
+import com.microsoft.azure.toolkit.lib.common.model.AbstractAzResourceModule;
+import com.microsoft.azure.toolkit.lib.common.model.AbstractAzServiceSubscription;
+import com.microsoft.azure.toolkit.lib.common.model.Region;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+public class EventHubsNamespaceSubscription extends AbstractAzServiceSubscription<EventHubsNamespaceSubscription, EventHubsManager> {
+    @Nonnull
+    private final String subscriptionId;
+    @Nonnull
+    private final EventHubsNamespaceModule eventHubsNamespaceModule;
+
+
+    protected EventHubsNamespaceSubscription(@Nonnull String subscriptionId, @Nonnull AzureEventHubsNamespace service) {
+        super(subscriptionId, service);
+        this.subscriptionId = subscriptionId;
+        this.eventHubsNamespaceModule = new EventHubsNamespaceModule(this);
+        getSubscription();
+    }
+
+    protected EventHubsNamespaceSubscription(@Nonnull EventHubsManager manager, @Nonnull AzureEventHubsNamespace service) {
+        this(manager.serviceClient().getSubscriptionId(), service);
+    }
+
+    public EventHubsNamespaceModule eventHubsNamespaces() {
+        return this.eventHubsNamespaceModule;
+    }
+
+    @Nonnull
+    @Override
+    public List<AbstractAzResourceModule<?, ?, ?>> getSubModules() {
+        return Collections.singletonList(eventHubsNamespaceModule);
+    }
+
+    @Override
+    public List<Region> listSupportedRegions(@NotNull String resourceType) {
+        return super.listSupportedRegions(this.eventHubsNamespaceModule.getName());
+    }
+}
