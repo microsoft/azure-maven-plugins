919a975f7068e0653aa873f228d85d266ec7aee7
yuwe <yuwe@microsoft.com>
2023-03-27 14:52:50 +0800

    Topic is not supported in basic pricing tier. Not show topic module when basic pricing tier. AB#2042024

--- azure-toolkit-libs/azure-toolkit-servicebus-lib/src/main/java/com/microsoft/azure/toolkit/lib/servicebus/ServiceBusNamespace.java
+++ azure-toolkit-libs/azure-toolkit-servicebus-lib/src/main/java/com/microsoft/azure/toolkit/lib/servicebus/ServiceBusNamespace.java
@@ -6,6 +6,7 @@
 package com.microsoft.azure.toolkit.lib.servicebus;
 
 import com.azure.resourcemanager.resources.fluentcore.arm.ResourceId;
+import com.azure.resourcemanager.servicebus.models.SkuTier;
 import com.microsoft.azure.toolkit.lib.common.model.AbstractAzResource;
 import com.microsoft.azure.toolkit.lib.common.model.AbstractAzResourceModule;
 import com.microsoft.azure.toolkit.lib.common.model.Deletable;
@@ -13,14 +14,16 @@ import com.microsoft.azure.toolkit.lib.servicebus.queue.ServiceBusQueueModule;
 import com.microsoft.azure.toolkit.lib.servicebus.topic.ServiceBusTopicModule;
 
 import javax.annotation.Nonnull;
-import java.util.Arrays;
-import java.util.List;
+import javax.annotation.Nullable;
+import java.util.*;
 
 public class ServiceBusNamespace extends AbstractAzResource<ServiceBusNamespace, ServiceBusNamespaceSubscription, com.azure.resourcemanager.servicebus.models.ServiceBusNamespace> implements Deletable {
     @Nonnull
     private final ServiceBusQueueModule queueModule;
     @Nonnull
     private final ServiceBusTopicModule topicModule;
+    @Nullable
+    private SkuTier skuTier;
     protected ServiceBusNamespace(@Nonnull String name, @Nonnull String resourceGroupName, @Nonnull ServiceBusNamespaceModule module) {
         super(name, resourceGroupName, module);
         this.queueModule = new ServiceBusQueueModule(this);
@@ -31,17 +34,28 @@ public class ServiceBusNamespace extends AbstractAzResource<ServiceBusNamespace,
         super(origin);
         this.queueModule = origin.queueModule;
         this.topicModule = origin.topicModule;
+        this.skuTier = origin.skuTier;
     }
 
     protected ServiceBusNamespace(@Nonnull com.azure.resourcemanager.servicebus.models.ServiceBusNamespace remote, @Nonnull ServiceBusNamespaceModule module) {
         super(remote.name(), ResourceId.fromString(remote.id()).resourceGroupName(), module);
         this.queueModule = new ServiceBusQueueModule(this);
         this.topicModule = new ServiceBusTopicModule(this);
+        this.skuTier = remote.sku().tier();
+    }
+
+    @Override
+    protected void updateAdditionalProperties(@Nullable com.azure.resourcemanager.servicebus.models.ServiceBusNamespace newRemote, @Nullable com.azure.resourcemanager.servicebus.models.ServiceBusNamespace oldRemote) {
+        super.updateAdditionalProperties(newRemote, oldRemote);
+        Optional.ofNullable(newRemote).ifPresent(r -> skuTier = r.sku().tier());
     }
 
     @Nonnull
     @Override
     public List<AbstractAzResourceModule<?, ?, ?>> getSubModules() {
+        if (Objects.equals(skuTier, SkuTier.BASIC)) {
+            return Collections.singletonList(queueModule);
+        }
         return Arrays.asList(queueModule, topicModule);
     }
 
