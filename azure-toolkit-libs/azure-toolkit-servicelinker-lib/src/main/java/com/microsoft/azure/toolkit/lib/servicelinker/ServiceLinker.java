cfc5636c60ffddee04237f3d3f6a7828c374cb9d
yuwe <yuwe@microsoft.com>
2023-04-17 16:27:25 +0800

    provide targetResourceId method

--- azure-toolkit-libs/azure-toolkit-servicelinker-lib/src/main/java/com/microsoft/azure/toolkit/lib/servicelinker/ServiceLinker.java
+++ azure-toolkit-libs/azure-toolkit-servicelinker-lib/src/main/java/com/microsoft/azure/toolkit/lib/servicelinker/ServiceLinker.java
@@ -1,15 +1,22 @@
 package com.microsoft.azure.toolkit.lib.servicelinker;
 
 import com.azure.resourcemanager.resources.fluentcore.arm.ResourceId;
+import com.azure.resourcemanager.servicelinker.models.AzureResource;
 import com.azure.resourcemanager.servicelinker.models.LinkerResource;
 import com.microsoft.azure.toolkit.lib.common.model.AbstractAzResource;
 import com.microsoft.azure.toolkit.lib.common.model.AbstractAzResourceModule;
+import lombok.Getter;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public class ServiceLinker extends AbstractAzResource<ServiceLinker, ServiceLinkerSubscription, LinkerResource> {
+    @Nullable
+    @Getter
+    private String targetResourceId = null;
     protected ServiceLinker(@Nonnull String name, @Nonnull String resourceGroupName, @Nonnull AbstractAzResourceModule<ServiceLinker, ServiceLinkerSubscription, LinkerResource> module) {
         super(name, resourceGroupName, module);
     }
@@ -20,6 +27,22 @@ public class ServiceLinker extends AbstractAzResource<ServiceLinker, ServiceLink
 
     protected ServiceLinker(@Nonnull LinkerResource remote, @Nonnull ServiceLinkerModule module) {
         super(remote.name(), ResourceId.fromString(remote.id()).resourceGroupName(), module);
+    }
+
+    @Override
+    protected void updateAdditionalProperties(@Nullable LinkerResource newRemote, @Nullable LinkerResource oldRemote) {
+        super.updateAdditionalProperties(newRemote, oldRemote);
+        Optional.ofNullable(newRemote).ifPresent(remote -> {
+            if (remote.targetService() instanceof AzureResource) {
+                this.targetResourceId = ((AzureResource) remote.targetService()).id();
+            }
+        });
+    }
+
+    @Override
+    public void invalidateCache() {
+        super.invalidateCache();
+        this.targetResourceId = null;
     }
 
     @Nonnull
