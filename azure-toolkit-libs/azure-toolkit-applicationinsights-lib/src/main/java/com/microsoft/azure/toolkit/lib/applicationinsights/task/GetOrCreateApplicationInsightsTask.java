788dbb3f925355c8274cfeb004325fec20a2b353
Flanker-MSFT <flanker-msft@LiudeMac-mini.local>
2024-01-30 10:07:38 +0800

    Support create workspace based application insight in function app creation task

--- azure-toolkit-libs/azure-toolkit-applicationinsights-lib/src/main/java/com/microsoft/azure/toolkit/lib/applicationinsights/task/GetOrCreateApplicationInsightsTask.java
+++ azure-toolkit-libs/azure-toolkit-applicationinsights-lib/src/main/java/com/microsoft/azure/toolkit/lib/applicationinsights/task/GetOrCreateApplicationInsightsTask.java
@@ -16,6 +16,7 @@ import com.microsoft.azure.toolkit.lib.monitor.LogAnalyticsWorkspaceConfig;
 import com.microsoft.azure.toolkit.lib.resource.AzureResources;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.Optional;
 
 public class GetOrCreateApplicationInsightsTask extends AzureTask<ApplicationInsight> {
@@ -32,10 +33,14 @@ public class GetOrCreateApplicationInsightsTask extends AzureTask<ApplicationIns
     private final String name;
     @Nonnull
     private final Region region;
-    @Nonnull
-    private final LogAnalyticsWorkspaceConfig workspaceConfig;
+    @Nullable
+    private LogAnalyticsWorkspaceConfig workspaceConfig;
 
-    public GetOrCreateApplicationInsightsTask(@Nonnull String subscriptionId, @Nonnull String resourceGroup, @Nonnull Region region, @Nonnull String name, @Nonnull LogAnalyticsWorkspaceConfig workspaceConfig) {
+    public GetOrCreateApplicationInsightsTask(@Nonnull String subscriptionId, @Nonnull String resourceGroup, @Nonnull Region region, @Nonnull String name) {
+        this(subscriptionId, resourceGroup, region, name, null);
+    }
+
+    public GetOrCreateApplicationInsightsTask(@Nonnull String subscriptionId, @Nonnull String resourceGroup, @Nonnull Region region, @Nonnull String name, @Nullable LogAnalyticsWorkspaceConfig workspaceConfig) {
         this.subscriptionId = subscriptionId;
         this.resourceGroup = resourceGroup;
         this.name = name;
@@ -45,6 +50,7 @@ public class GetOrCreateApplicationInsightsTask extends AzureTask<ApplicationIns
 
     @Override
     public ApplicationInsight doExecute() {
+        workspaceConfig = Optional.ofNullable(workspaceConfig).orElseGet(this::getDefaultWorkspaceConfig);
         Azure.az(AzureResources.class).groups(subscriptionId).createResourceGroupIfNotExist(this.resourceGroup, this.region);
         final ApplicationInsightsModule insightsModule = Azure.az(AzureApplicationInsights.class).applicationInsights(subscriptionId);
         return Optional.ofNullable(insightsModule.get(name, this.resourceGroup)).orElseGet(() -> {
@@ -53,5 +59,14 @@ public class GetOrCreateApplicationInsightsTask extends AzureTask<ApplicationIns
             draft.setWorkspaceConfig(workspaceConfig);
             return draft.commit();
         });
+    }
+
+    private LogAnalyticsWorkspaceConfig getDefaultWorkspaceConfig() {
+        final String defaultWorkspaceName = String.format("DefaultWorkspace-%s-%s", subscriptionId, region.getAbbreviation());
+        return LogAnalyticsWorkspaceConfig.builder().newCreate(true)
+            .subscriptionId(subscriptionId)
+            .name(defaultWorkspaceName)
+            .regionName(region.getName())
+            .build();
     }
 }
