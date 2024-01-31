309743b45c0abd25a3cd8ddf9c0ecc4f4d4dd617
Flanker32 <hanli@microsoft.com>
2024-01-31 14:20:05 +0800

    Set diagnostic configuration only for service plan function apps

--- azure-toolkit-libs/azure-toolkit-appservice-lib/src/main/java/com/microsoft/azure/toolkit/lib/appservice/function/FunctionAppDraft.java
+++ azure-toolkit-libs/azure-toolkit-appservice-lib/src/main/java/com/microsoft/azure/toolkit/lib/appservice/function/FunctionAppDraft.java
@@ -164,7 +164,8 @@ public class FunctionAppDraft extends FunctionApp implements AzResource.Draft<Fu
         if (Objects.nonNull(storageAccount)) {
             withCreate.withExistingStorageAccount(storageAccount.getRemote());
         }
-        if (Objects.nonNull(newDiagnosticConfig)) {
+        // diagnostic config is only available for service plan function apps
+        if (Objects.nonNull(newDiagnosticConfig) && Objects.isNull(environment)) {
             AppServiceUtils.defineDiagnosticConfigurationForWebAppBase(withCreate, newDiagnosticConfig);
         }
         final Boolean enableDistributedTracing = ensureConfig().getEnableDistributedTracing();
@@ -280,7 +281,7 @@ public class FunctionAppDraft extends FunctionApp implements AzResource.Draft<Fu
             Optional.of(settingsToAdd).filter(ignore -> isAppSettingsModified).ifPresent(update::withAppSettings);
             Optional.of(settingsToRemove).filter(CollectionUtils::isNotEmpty).filter(ignore -> isAppSettingsModified).ifPresent(s -> s.forEach(update::withoutAppSetting));
             Optional.ofNullable(newDockerConfig).filter(ignore -> dockerModified).ifPresent(p -> updateDockerConfiguration(update, p));
-            Optional.ofNullable(newDiagnosticConfig).filter(ignore -> isDiagnosticConfigModified).ifPresent(c -> AppServiceUtils.updateDiagnosticConfigurationForWebAppBase(update, c));
+            Optional.ofNullable(newDiagnosticConfig).filter(ignore -> isDiagnosticConfigModified).filter(ignore -> StringUtils.isBlank(getEnvironmentId())).ifPresent(c -> AppServiceUtils.updateDiagnosticConfigurationForWebAppBase(update, c));
             Optional.ofNullable(newFlexConsumptionConfiguration).filter(ignore -> flexConsumptionModified).ifPresent(c -> update.withContainerSize(c.getInstanceSize()));
             Optional.ofNullable(storageAccount).ifPresent(s -> update.withExistingStorageAccount(s.getRemote()));
             final IAzureMessager messager = AzureMessager.getMessager();
